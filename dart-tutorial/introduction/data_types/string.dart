777e94e81ce67128b999605f9e5305be3fda222a
pchchv <ipchchv@gmail.com>
2024-02-02 16:44:59 +0300

    Add a special char and raw string syntax example

--- dart-tutorial/introduction/data_types/string.dart
+++ dart-tutorial/introduction/data_types/string.dart
@@ -23,4 +23,15 @@ I am also writing here.
     // Printing Information
     print("Multiline text is $multiLineText");
     print("Other multiline text is $otherMultiLineText");
+
+    print("I am from \nUS.");
+    print("I am from \tUS.");
+
+    // Set prize value
+    num prize = 10;
+    String withoutRawString = "The value of prize is \t $prize"; // regular String
+    String withRawString =r"The value of prize is \t $prize"; // raw String
+    
+    print("Without Raw: $withoutRawString"); // regular result
+    print("With Raw: $withRawString"); // with raw result
 }
