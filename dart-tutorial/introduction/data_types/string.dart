adb433f29b13d7df21fa31e0e00dbebb33c86c4c
pchchv <ipchchv@gmail.com>
2024-02-02 16:07:45 +0300

    Add a multi-line string syntax example

--- dart-tutorial/introduction/data_types/string.dart
+++ dart-tutorial/introduction/data_types/string.dart
@@ -4,5 +4,23 @@ void main() {
     String address = "New York 2140";
     
     // Printing Values
-    print("School name is $schoolName and address is $address");   
+    print("School name is $schoolName and address is $address");
+    
+    // Multi Line Using Single Quotes
+    String multiLineText = '''
+This is Multi Line Text
+with 3 single quote
+I am also writing here.
+''';
+    
+    // Multi Line Using Double Quotes
+    String otherMultiLineText = """
+This is Multi Line Text
+with 3 double quote
+I am also writing here.
+""";
+
+    // Printing Information
+    print("Multiline text is $multiLineText");
+    print("Other multiline text is $otherMultiLineText");
 }
