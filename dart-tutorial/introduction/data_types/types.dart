b1370137e2b25cf47bc92adb7b268a39d2009f71
pchchv <ipchchv@gmail.com>
2024-02-03 12:59:05 +0300

    lens config

--- dart-tutorial/introduction/data_types/types.dart
+++ dart-tutorial/introduction/data_types/types.dart
@@ -1,11 +1,12 @@
 void main() { 
     var a = 10;
     print(a.runtimeType); 
+    // ignore: unnecessary_type_check
     print(a is int); // true
     
-    var myVariable = 50; // You can also use int instead of var
-    myVariable = "Hello"; // this will give error
-    print(myVariable);
+    // var myVariable = 50; // You can also use int instead of var
+    // myVariable = "Hello"; // this will give error
+    // print(myVariable);
     
     dynamic myVar = 50;
     myVar = "Hello";
