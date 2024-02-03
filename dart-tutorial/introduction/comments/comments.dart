fc99b0ab4f0175b163b186907e35fd64139d66dc
pchchv <ipchchv@gmail.com>
2024-02-03 16:07:47 +0300

    Add a comments syntax example

---
+++ dart-tutorial/introduction/comments/comments.dart
@@ -0,0 +1,11 @@
+void main() {
+    /// This is documentation comment
+    // This is single-line comment.
+    /*
+    This is a multi-line comment.
+    */
+    print("Welcome to Technology Channel.");
+    /*
+    This is a multi-line comment.
+    */
+}
