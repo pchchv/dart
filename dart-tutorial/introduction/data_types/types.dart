7b86296fe6b8cb0485b254b7d795b25626626954
pchchv <ipchchv@gmail.com>
2024-02-03 12:57:36 +0300

    Add a typed syntax example

--- dart-tutorial/introduction/data_types/types.dart
+++ dart-tutorial/introduction/data_types/types.dart
@@ -2,4 +2,12 @@ void main() {
     var a = 10;
     print(a.runtimeType); 
     print(a is int); // true
+    
+    var myVariable = 50; // You can also use int instead of var
+    myVariable = "Hello"; // this will give error
+    print(myVariable);
+    
+    dynamic myVar = 50;
+    myVar = "Hello";
+    print(myVar);
 }
