cd46218f0b6e6a9d712231c4b4e34f1dd4cf8284
pchchv <ipchchv@gmail.com>
2024-02-04 17:32:21 +0300

    Add a logical operators syntax examples

--- dart-tutorial/introduction/operators/relational&logical_operators.dart
+++ dart-tutorial/introduction/operators/relational&logical_operators.dart
@@ -1,11 +1,21 @@
 void main() {
-    int num1=10;
-    int num2=5;
+    int num1 = 10;
+    int num2 = 5;
     
     // printing info
-    print(num1==num2);
-    print(num1<num2);
-    print(num1>num2);
-    print(num1<=num2);
-    print(num1>=num2);
+    print(num1 == num2);
+    print(num1 <  num2);
+    print(num1 >  num2);
+    print(num1 <= num2);
+    print(num1 >= num2);
+
+    int userid = 123;
+    int userpin = 456;
+
+    // printing Info
+    print((userid == 123) && (userpin == 456));  // print true
+    print((userid == 1213) && (userpin == 456)); // print false.
+    print((userid == 123) || (userpin == 456));  // print true.
+    print((userid == 1213) || (userpin == 456)); // print true
+    print((userid == 123) != (userpin == 456));  // print false
 }
