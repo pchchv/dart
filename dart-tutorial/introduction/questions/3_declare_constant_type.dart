5c631e4a7e50033ddb35d1ff263adce38029b79f
pchchv <ipchchv@gmail.com>
2024-02-06 16:34:20 +0300

    Solve questions 1-4

---
+++ dart-tutorial/introduction/questions/3_declare_constant_type.dart
@@ -0,0 +1,7 @@
+/// Declare constant type of int set value 7.
+
+void main() {
+    const int num = 7;
+    // num = 8; // error
+    print(num);
+}
---
+++ dart-tutorial/introduction/questions/4_simple_interest.dart
@@ -0,0 +1,15 @@
+/// Write a program in Dart that finds simple interest. Formula = (p * t * r) / 100
+
+import 'dart:io';
+
+void main() {
+    stdout.write("Enter the persent value:");
+    int? p = int.parse(stdin.readLineSync()!);
+    stdout.write("Enter the interest rate:");
+    int? r = int.parse(stdin.readLineSync()!);
+    stdout.write("Enter the time period:");
+    int? t = int.parse(stdin.readLineSync()!);
+
+    var si = (p * r * t) / 100;
+    print("Simple interest is ${si}");
+}
