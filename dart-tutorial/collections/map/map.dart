6ea86905322c0746d99dfb6465cf2b0c11ee0eab
pchchv <ipchchv@gmail.com>
2024-02-11 20:28:44 +0300

    Add a map usage syntax examples

---
+++ dart-tutorial/collections/map/map.dart
@@ -0,0 +1,9 @@
+void main() {
+    Map<String, String> countryCapital = {
+        'USA': 'Washington, D.C.',
+        'India': 'New Delhi',
+        'China': 'Beijing'
+    };
+    print(countryCapital);
+    print(countryCapital["USA"]);
+}
