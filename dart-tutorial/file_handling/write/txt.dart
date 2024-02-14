c87a1302ee46a2cd43286ef5d8eb520104413982
pchchv <ipchchv@gmail.com>
2024-02-14 10:41:56 +0300

    Fix

--- dart-tutorial/file_handling/write/txt.dart
+++ dart-tutorial/file_handling/write/txt.dart
@@ -2,20 +2,20 @@
 import 'dart:io';
 
 void add() {
-  // open file
-  File file =  File('test.txt');
-  // write to file
-  file.writeAsStringSync('\nThis is a new content.', mode: FileMode.append);
-  print('Congratulations!! New content is added on top of previous content.');
+    // open file
+    File file =  File('test.txt');
+    // write to file
+    file.writeAsStringSync('\nThis is a new content.', mode: FileMode.append);
+    print('Congratulations!! New content is added on top of previous content.');
 }
 
 
 void main() {
-  // open file
-  File file = File('test.txt');
-  // write to file
-  file.writeAsStringSync('Welcome to test.txt file.');
-  print('File written.');
+    // open file
+    File file = File('test.txt');
+    // write to file
+    file.writeAsStringSync('Welcome to test.txt file.');
+    print('File written.');
 
-  add();
+    add();
 }
