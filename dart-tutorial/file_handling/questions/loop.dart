05ef6fae7d13358457e47e08c4990074e98c52ca
pchchv <ipchchv@gmail.com>
2024-02-14 12:31:38 +0300

    Solve question 5

---
+++ dart-tutorial/file_handling/questions/loop.dart
@@ -0,0 +1,9 @@
+/// 5, Write a dart program to create 100 files using loop.
+import 'dart:io';
+
+void main() {
+    Directory dir = Directory.current;
+    for (int i = 0; i < 100; i++) {
+        new File("${dir.path}/loop/loop${i}.txt").createSync();
+    }
+}
