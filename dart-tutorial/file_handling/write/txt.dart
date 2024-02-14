dec669e068448cb9ebeff8787d979e9fd347afe8
pchchv <ipchchv@gmail.com>
2024-02-14 10:41:16 +0300

    Add an example of writing a text file

---
+++ dart-tutorial/file_handling/write/txt.dart
@@ -0,0 +1,21 @@
+// dart program to write to file
+import 'dart:io';
+
+void add() {
+  // open file
+  File file =  File('test.txt');
+  // write to file
+  file.writeAsStringSync('\nThis is a new content.', mode: FileMode.append);
+  print('Congratulations!! New content is added on top of previous content.');
+}
+
+
+void main() {
+  // open file
+  File file = File('test.txt');
+  // write to file
+  file.writeAsStringSync('Welcome to test.txt file.');
+  print('File written.');
+
+  add();
+}
