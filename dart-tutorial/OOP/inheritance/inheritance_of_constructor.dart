92fc91b4c36e1a735c5584bf53a46f943791709f
pchchv <ipchchv@gmail.com>
2024-02-18 18:30:53 +0300

    Add a example of an inheritance of constructor with parameters

--- dart-tutorial/OOP/inheritance/inheritance_of_constructor.dart
+++ dart-tutorial/OOP/inheritance/inheritance_of_constructor.dart
@@ -14,7 +14,45 @@ class MacBook extends Laptop {
     }
 }
 
+/*
+class MacBook extends Laptop {
+    // Constructor
+    MacBook({String name, String color}) : super(name: name, color: color) {
+        print("MacBook constructor");
+    }
+}
+*/
+
+class Person {
+    String name;
+    int age;
+
+    // Constructor
+    Person(this.name, this.age);
+}
+
+class Student extends Person {
+    int rollNumber;
+
+    // Constructor
+    Student(String name, int age, this.rollNumber) : super(name, age);
+}
+
 void main() {
     // ignore: unused_local_variable
     var macbook = MacBook("MacBook Pro", "Silver");
+
+    print("\n-------------------\n");
+    
+    var student = Student("John", 20, 1);
+    print("Student name: ${student.name}");
+    print("Student age: ${student.age}");
+    print("Student roll number: ${student.rollNumber}");
+
+    /*
+    print("\n-------------------\n");
+
+    // ignore: unused_local_variable
+    var macbook = MacBook(name: "MacBook Pro", color: "Silver");
+    */
 }
