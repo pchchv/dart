49202ddeea84a986ad05f678b3f2206f75871e23
pchchv <ipchchv@gmail.com>
2024-02-20 15:08:17 +0300

    Solve question 3

---
+++ dart-tutorial/OOP/questions/genders.dart
@@ -0,0 +1,13 @@
+/// 3. Write a dart program to create an enum class for gender [male, female, others] and print all values.
+
+enum Gender {
+    Male,
+    Female,
+    Other
+}
+
+void main() {
+    print(Gender.Male);
+    print(Gender.Female);
+    print(Gender.Other);
+}
