ad4f45b86b39794098e38fcacd7cf92443d498ab
pchchv <ipchchv@gmail.com>
2024-02-20 13:25:16 +0300

    Add an examples of a keyword in mixin

--- dart-tutorial/OOP/mixin.dart
+++ dart-tutorial/OOP/mixin.dart
@@ -33,6 +33,30 @@ class Bird with CanFly, CanWalk {
 class Human with CanWalk { 
 }
 
+abstract class Animal {
+    // properties
+    String name;
+    double speed;
+
+    // constructor
+    Animal(this.name, this.speed);
+
+    // abstract method
+    void run();
+}
+
+// mixin CanRun is only used by class that extends Animal
+mixin CanRun on Animal {
+    // implementation of abstract method
+    @override
+    void run() => print('$name is Running at speed $speed');
+}
+
+class Dog extends Animal with CanRun {
+    // constructor
+    Dog(String name, double speed) : super(name, speed);
+}
+
 void main() {
     var car = Car();
     car.electricVariant();
@@ -46,4 +70,9 @@ void main() {
 
     var human = Human();
     human.walk();
+
+    print("\n-------------------\n");
+
+    var dog = Dog('My Dog', 25);
+    dog.run();
 }
