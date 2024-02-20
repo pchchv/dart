567919dfa68d67cc7d1060cc38b5c667480be34b
pchchv <ipchchv@gmail.com>
2024-02-20 12:48:29 +0300

    Add a enum examples

---
+++ dart-tutorial/OOP/enum.dart
@@ -0,0 +1,75 @@
+enum days {
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+
+enum Gender {
+    Male,
+    Female,
+    Other
+}
+
+class Person {
+  // Properties
+  String? firstName;
+  String? lastName;
+  Gender? gender;
+
+  // Constructor
+  Person(this.firstName, this.lastName, this.gender);
+
+  // display() method
+  void display() {
+    print("First Name: $firstName");
+    print("Last Name: $lastName");
+    print("Gender: $gender");
+  }
+}
+
+
+void main() {
+    var today = days.Friday;
+    switch (today) {
+        case days.Sunday:
+        print("Today is Sunday.");
+        break;
+        case days.Monday:
+        print("Today is Monday.");
+        break;
+        case days.Tuesday:
+        print("Today is Tuesday.");
+        break;
+        case days.Wednesday:
+        print("Today is Wednesday.");
+        break;
+        case days.Thursday:
+        print("Today is Thursday.");
+        break;
+        case days.Friday:
+        print("Today is Friday.");
+        break;
+        case days.Saturday:
+        print("Today is Saturday.");
+        break;
+    }
+
+    print("\n-------------------\n");
+
+    Person p1 = Person("John", "Doe", Gender.Male);
+    p1.display();
+
+    Person p2 = Person("Menuka", "Sharma", Gender.Female);
+    p2.display();
+
+    print("\n-------------------\n");
+
+    // Days.values: It returns all the values of the enum.
+    for (days day in days.values) {
+        print(day);
+    }
+}
