a735ddde3fb30a95ee6e636d8481c66048e1d6c5
pchchv <ipchchv@gmail.com>
2024-02-20 13:13:59 +0300

    Add a multiple interface syntax example

--- dart-tutorial/OOP/interface.dart
+++ dart-tutorial/OOP/interface.dart
@@ -99,6 +99,36 @@ class Student2 implements CalculateTotal, CalculateAverage {
     }
 }
 
+// abstract class as interface
+abstract class Area {
+    void area();
+}
+
+// abstract class as interface
+abstract class Perimeter {
+    void perimeter();
+}
+
+// implements multiple interfaces
+class Rectangle implements Area, Perimeter {
+    // properties
+    int length, breadth;
+    // constructor
+    Rectangle(this.length, this.breadth);
+
+    // implementation of area()
+    @override
+    void area() {
+        print('The area of the rectangle is ${length * breadth}');
+    }
+
+    // implementation of perimeter()
+    @override
+    void perimeter() {
+        print('The perimeter of the rectangle is ${2 * (length + breadth)}');
+    }
+}
+
 void main() {
     var macBook = MacBook();
     macBook.turnOn();
@@ -123,4 +153,10 @@ void main() {
     Student2 student2 = Student2(90, 80, 70);
     print('Total marks: ${student2.total()}');
     print('Average marks: ${student2.average()}');
+
+    print("\n-------------------\n");
+
+    Rectangle rectangle = Rectangle(10, 20);
+    rectangle.area();
+    rectangle.perimeter();
 }
