f5ab4869576e8b5996ccbcb62051f180b2768131
pchchv <ipchchv@gmail.com>
2024-03-03 16:53:57 +0300

    Add examples of creating streams

---
+++ dart-tutorial/asynchronous_programming/streams/create.dart
@@ -0,0 +1,14 @@
+// function that returns a stream
+Stream<String> getUserName() async* {
+    await Future.delayed(Duration(seconds: 1));
+    yield 'Mark';
+    await Future.delayed(Duration(seconds: 1));
+    yield 'John';
+    await Future.delayed(Duration(seconds: 1));
+    yield 'Smith';
+}
+
+// function that returns a stream
+Stream<String> getUserName2() {
+  return Stream.fromIterable(['Mark', 'John', 'Smith']);
+}
