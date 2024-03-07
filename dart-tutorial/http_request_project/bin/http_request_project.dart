7a41c7478ad02096a35087d12b861e9974cacf04
pchchv <ipchchv@gmail.com>
2024-03-07 16:18:08 +0300

    Create project

---
+++ dart-tutorial/http_request_project/bin/http_request_project.dart
@@ -0,0 +1,5 @@
+import 'package:http_request_project/http_request_project.dart' as http_request_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${http_request_project.calculate()}!');
+}
---
+++ dart-tutorial/http_request_project/test/http_request_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:http_request_project/http_request_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
