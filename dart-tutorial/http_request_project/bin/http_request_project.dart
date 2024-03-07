281fb5ca328ef87b696b345aaab0696dd1a12d17
pchchv <ipchchv@gmail.com>
2024-03-07 16:21:37 +0300

    Make HTTP Post Request

--- dart-tutorial/http_request_project/bin/http_request_project.dart
+++ dart-tutorial/http_request_project/bin/http_request_project.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 
 
-void main() async {
+void get() async {
     var url = Uri.parse('https://jsonplaceholder.typicode.com/posts/1');
     // make http get request
     var response = await http.get(url);
@@ -11,4 +11,21 @@ void main() async {
     } else {
         print('Request failed with status: ${response.statusCode}.');
     }
+}
+
+void post() async {
+    var url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    // make http post request
+    var response = await http.post(url, body: {'title': 'foo', 'body': 'bar', 'userId': '1'});
+    // check the status code for the result  
+    if (response.statusCode == 201) {
+        print(response.body);
+    } else {
+        print('Request failed with status: ${response.statusCode}.');
+    }  
+}
+
+void main() {
+    get();
+    post();
 }
