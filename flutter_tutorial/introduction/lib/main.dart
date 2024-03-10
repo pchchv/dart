ba30999c2505d9c28e8a54f690a7b3331544dbc9
pchchv <ipchchv@gmail.com>
2024-03-10 20:42:06 +0300

    Create Container Boxes

--- flutter_tutorial/introduction/lib/main.dart
+++ flutter_tutorial/introduction/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+/*
 void main() {
   runApp(
     MaterialApp(
@@ -12,6 +13,30 @@ void main() {
             Icon(Icons.star, size: 50),
             Text('I am learning flutter'),
             Icon(Icons.star, size: 50),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+*/
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Column In Flutter'),
+        ),
+        body: Column(
+          children: [
+            Container(height: 100, width: 100, color: Colors.blue),
+            const SizedBox(height: 5),
+            Container(height: 100, width: 100, color: Colors.blue),
+            const SizedBox(height: 5),
+            Container(height: 100, width: 100, color: Colors.blue),
+            const SizedBox(height: 5),
+            Container(height: 100, width: 100, color: Colors.blue),
           ],
         ),
       ),
