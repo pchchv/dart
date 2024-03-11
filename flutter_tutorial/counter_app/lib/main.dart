454e680ab2896f4e7948aebe8bd06c2ef47b86a6
pchchv <ipchchv@gmail.com>
2024-03-11 13:51:01 +0300

    Create the CounterApp widget

--- flutter_tutorial/counter_app/lib/main.dart
+++ flutter_tutorial/counter_app/lib/main.dart
@@ -3,3 +3,14 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const CounterApp());
 }
+
+class CounterApp extends StatelessWidget {
+  const CounterApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter App',
+      home: CounterScreen(),
+    );
+  }
+}
