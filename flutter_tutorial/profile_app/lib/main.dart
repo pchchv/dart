c84e1def90b6e0dd5141ac770ff9ba69aeec6c0e
pchchv <ipchchv@gmail.com>
2024-03-14 16:45:52 +0300

    Implement ProfileApp

--- flutter_tutorial/profile_app/lib/main.dart
+++ flutter_tutorial/profile_app/lib/main.dart
@@ -1,3 +1,14 @@
-void main() {
- // runApp(const ProfileApp());
+import 'package:flutter/material.dart';
+
+void main() => runApp(const ProfileApp());
+
+class ProfileApp extends StatelessWidget {
+  const ProfileApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
 }
