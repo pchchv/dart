721aec3813a221a3a56cfc363da2ece201206abe
pchchv <ipchchv@gmail.com>
2024-03-20 13:34:15 +0300

    Add a _MenuPageState

--- flutter_tutorial/restaurant_app/lib/main.dart
+++ flutter_tutorial/restaurant_app/lib/main.dart
@@ -1,2 +1,10 @@
 void main() {
 }
+
+class _MenuPageState extends State<MenuPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
