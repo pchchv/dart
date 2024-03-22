41f21170bc33e3e22022fd00ffd7712ab4ce1e50
pchchv <ipchchv@gmail.com>
2024-03-22 08:54:20 +0300

    Put the dishes on a specific list

--- flutter_tutorial/restaurant_app/lib/main.dart
+++ flutter_tutorial/restaurant_app/lib/main.dart
@@ -22,15 +22,28 @@ class MenuPage extends StatefulWidget {
 }
 
 class _MenuPageState extends State<MenuPage> {
+  final List<ListTile> items = [
+    ListTile(
+      title: const Text('Dish Description'),
+      subtitle: const Text('8.33'),
+      leading: SizedBox(child: Image.network('https://picsum.photos/250?image=1')),
+    ),
+    ListTile(
+      title: const Text('Dish Description'),
+      subtitle: const Text('8.33'),
+      leading: SizedBox(child: Image.network('https://picsum.photos/250?image=2')),
+    ),
+    ListTile(
+      title: const Text('Dish Description'),
+      subtitle: const Text('8.33'),
+      leading: SizedBox(child: Image.network('https://picsum.photos/250?image=3')),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return ListView(
       scrollDirection: Axis.horizontal,
-      children: [
-        Image.network('https://picsum.photos/250?image=1'),
-        Image.network('https://picsum.photos/250?image=2'),
-        Image.network('https://picsum.photos/250?image=3'),
-      ],
     );
   }
 }
