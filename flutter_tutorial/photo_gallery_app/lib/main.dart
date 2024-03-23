21ebdb44b98d7e305b41985dfd3c7d4a5c7dad96
pchchv <ipchchv@gmail.com>
2024-03-23 14:54:25 +0300

    Create basic gallery

--- flutter_tutorial/photo_gallery_app/lib/main.dart
+++ flutter_tutorial/photo_gallery_app/lib/main.dart
@@ -1,1 +1,25 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('GridView')),
+        body: GridView.count(
+          crossAxisCount: 3,
+          children: [
+            Image.network('https://picsum.photos/200?image=25'),
+            // Add more images as needed
+          ],
+        ),
+      ),
+    );
+  }
+}
