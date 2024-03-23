7128a8fff03b8b6e9a4076b1468f6c2e63cf299c
pchchv <ipchchv@gmail.com>
2024-03-23 14:54:57 +0300

    Refactoring

--- flutter_tutorial/photo_gallery_app/lib/main.dart
+++ flutter_tutorial/photo_gallery_app/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const GalleryApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GalleryApp extends StatelessWidget {
+  const GalleryApp({super.key});
 
   @override
   Widget build(BuildContext context) {
