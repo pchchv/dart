2301db3a11b892891e51332ba79047c4ab1089e6
pchchv <ipchchv@gmail.com>
2024-04-02 18:04:38 +0300

    Implement a FriendsApp

--- flutter_tutorial/friends_app/lib/main.dart
+++ flutter_tutorial/friends_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class Friend {
   final String name;
 
@@ -13,3 +15,31 @@ List<Friend> posts = [
 ];
 
 void main() {}
+
+class FriendsApp extends StatelessWidget {
+  const FriendsApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Friends list")),
+        body: ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ListTile(
+                    title: Text(posts[index].name),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
