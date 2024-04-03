d0e47b877ba53fcb2d4a2f8a2707b7831224ebbb
pchchv <ipchchv@gmail.com>
2024-04-03 15:09:21 +0300

    Implement a ChessApp with GridView

--- flutter_tutorial/chess_app/lib/main.dart
+++ flutter_tutorial/chess_app/lib/main.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
+
 void main() {}
+
+class ChessApp extends StatelessWidget {
+  const ChessApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: const Text('Photo Gallery')),
+          body: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
+            itemCount: 1,
+            itemBuilder: (BuildContext context, int index) {
+              return const Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                ),
+              );
+            },
+          )),
+    );
+  }
+}
