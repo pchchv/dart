eee7863b50ee7a2b82dc6d6b5b14c80d927bee88
pchchv <ipchchv@gmail.com>
2024-04-03 15:57:03 +0300

    Refactoring

--- flutter_tutorial/chess_app/lib/main.dart
+++ flutter_tutorial/chess_app/lib/main.dart
@@ -18,18 +18,19 @@ class _ChessState extends State<ChessApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: const Text('Photo Gallery')),
-          body: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),
-            itemCount: 64,
-            itemBuilder: (BuildContext context, int index) {
-              return const Card(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                ),
-              );
-            },
-          )),
+        appBar: AppBar(title: const Text('Chess board')),
+        body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),
+          itemCount: 64,
+          itemBuilder: (BuildContext context, int index) {
+            return const Card(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+              ),
+            );
+          },
+        )
+      ),
     );
   }
 }
