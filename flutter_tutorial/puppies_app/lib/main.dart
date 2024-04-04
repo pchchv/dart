d4f8c9f5656b5d2d96421be6c7e928699b978c2b
pchchv <ipchchv@gmail.com>
2024-04-04 13:13:34 +0300

    Implement a PuppiesApp

--- flutter_tutorial/puppies_app/lib/main.dart
+++ flutter_tutorial/puppies_app/lib/main.dart
@@ -1,1 +1,19 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() => runApp(const PuppiesApp());
+
+class PuppiesApp extends StatelessWidget {
+  const PuppiesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Puppies App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Puppies App"),
+        ),
+      ),
+    );
+  }
+}
