f83e01f372e61547b7984ee84ea067587daf888f
pchchv <ipchchv@gmail.com>
2024-04-05 15:14:10 +0300

    Implement a ListView

--- flutter_tutorial/text_app/lib/main.dart
+++ flutter_tutorial/text_app/lib/main.dart
@@ -6,4 +6,47 @@ void main() {
 
 class TextApp extends StatelessWidget {
   const TextApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('ListView')),
+        body: ListView(
+          children: const[
+            ListTile(
+              title: Text("First line"),
+            ),
+            ListTile(
+              title: Text("Second line"),
+            ),
+            ListTile(
+              title: Text("Third line"),
+            ),
+            ListTile(
+              title: Text("Fourth line"),
+            ),
+            ListTile(
+              title: Text("Fifth line"),
+            ),
+            ListTile(
+              title: Text("Sixth line"),
+            ),
+            ListTile(
+              title: Text("Seventh line"),
+            ),
+            ListTile(
+              title: Text("Eighth line"),
+            ),
+            ListTile(
+              title: Text("Ninth line"),
+            ),
+            ListTile(
+              title: Text("Tenth line"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
