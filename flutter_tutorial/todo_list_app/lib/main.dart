8e7fa9dc7a5c345513fa107f0fbef8d7c39ae4e7
pchchv <ipchchv@gmail.com>
2024-04-05 15:01:39 +0300

    Implement a ToDoApp

--- flutter_tutorial/todo_list_app/lib/main.dart
+++ flutter_tutorial/todo_list_app/lib/main.dart
@@ -1,1 +1,34 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ToDoApp());
+}
+
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('ListView')),
+        body: ListView(
+          children: const [
+            ListTile(
+              title: Text('Go to Gym'),
+              subtitle: Text('Go to Gym at 6:00 AM'),
+            ),
+            ListTile(
+              title: Text('Go to College'),
+              subtitle: Text('Go to College at 8:00 AM'),
+            ),
+            ListTile(
+              title: Text('Go to Office'),
+              subtitle: Text('Go to Office at 11:00 AM'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
