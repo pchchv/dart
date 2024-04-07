be03b42a6bea47145541d817384b0ca9e2c184eb
pchchv <ipchchv@gmail.com>
2024-04-07 16:16:43 +0300

    Implement NumListApp

--- flutter_tutorial/num_list_app/lib/main.dart
+++ flutter_tutorial/num_list_app/lib/main.dart
@@ -1,1 +1,22 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() => runApp(const NumListApp());
+
+class NumListApp extends StatelessWidget {
+  const NumListApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Numbers List App")),
+        body: ListView.builder (
+          itemCount: 50,
+          itemBuilder: (BuildContext ctxt, int index) {
+            return Text(index.toString());
+          }
+        )
+      ),
+    );
+  }
+}
