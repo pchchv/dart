00ddf5f97540a51e7dded4c9747e4a3a3a41522c
pchchv <ipchchv@gmail.com>
2024-04-10 15:32:40 +0300

    Implement a GridViewW

--- flutter_tutorial/grid_view_app/lib/main.dart
+++ flutter_tutorial/grid_view_app/lib/main.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/material.dart';
+
 void main() {}
+
+class GridViewW extends StatelessWidget {
+  const GridViewW({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 5,
+      ),
+      itemCount: 20,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          margin: const EdgeInsets.all(5),
+          color: Colors.primaries[index % Colors.primaries.length],
+          child: Center(
+            child: Text(
+              'Item ${index + 1}',
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
