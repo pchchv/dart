8aa760a8aa53b7977f72c89d013f529a96fdc6a3
pchchv <ipchchv@gmail.com>
2024-04-10 15:21:25 +0300

    Implement an OuterListView

--- flutter_tutorial/list_views_app/lib/main.dart
+++ flutter_tutorial/list_views_app/lib/main.dart
@@ -2,6 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() {}
 
+class OuterListView extends StatelessWidget {
+  const OuterListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 3,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text('Outer List Item ${index + 1}'),
+          subtitle: const InnerListView(),
+        );
+      },
+    );
+  }
+}
+
 class InnerListView extends StatelessWidget {
   const InnerListView({super.key});
 
