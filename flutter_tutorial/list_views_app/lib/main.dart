1d13fc391b13cda70445cfccf45849357fad822f
pchchv <ipchchv@gmail.com>
2024-04-10 15:20:43 +0300

    Implement an InnerListView

--- flutter_tutorial/list_views_app/lib/main.dart
+++ flutter_tutorial/list_views_app/lib/main.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+
 void main() {}
+
+class InnerListView extends StatelessWidget {
+  const InnerListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (BuildContext context, int index) {
+          return Text('Inner List Item ${index + 1}', style: const TextStyle(color: Colors.red));
+        },
+      ),
+    );
+  }
+}
