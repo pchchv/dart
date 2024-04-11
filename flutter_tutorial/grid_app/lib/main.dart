775b20f31502e3d7b9860836019aa9c2bf69d407
pchchv <ipchchv@gmail.com>
2024-04-11 12:56:08 +0300

    Implement _buildSquareItem

--- flutter_tutorial/grid_app/lib/main.dart
+++ flutter_tutorial/grid_app/lib/main.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+
 void main() {}
+
+class AspectRatioGridView extends StatelessWidget {
+  const AspectRatioGridView({super.key});
+
+  Widget _buildSquareItem(int index) {
+    return Container(
+      width: 50,
+      height: 50,
+      color: Colors.blue,
+      child: Center(
+        child: Text(
+          'Item $index',
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
