7030bbee4863f90cccffe319e07353e2c9bbf5f7
pchchv <ipchchv@gmail.com>
2024-04-11 18:08:48 +0300

    Implement a _scrollListener and _loadMoreItems

--- flutter_tutorial/inf_scroll_app/lib/main.dart
+++ flutter_tutorial/inf_scroll_app/lib/main.dart
@@ -1,6 +1,30 @@
 void main() {}
 
 class _InfiniteScrollingListViewState extends State<InfiniteScrollingListView> {
+
+  void _scrollListener() {
+    if (_scrollController.offset >=
+            _scrollController.position.maxScrollExtent &&
+        !_scrollController.position.outOfRange) {
+      if (_currentPage < _totalPages && !_isLoading) {
+        _loadMoreItems();
+      }
+    }
+  }
+
+  void _loadMoreItems() {
+    setState(() {
+      _isLoading = true;
+    });
+
+    Future.delayed(const Duration(seconds: 2), () {
+      setState(() {
+        _items.addAll(List.generate(20, (index) => index + _items.length));
+        _isLoading = false;
+        _currentPage++;
+      });
+    });
+  }
   Widget _buildLoadingIndicator() {
     return const Padding(
       padding: EdgeInsets.all(8.0),
