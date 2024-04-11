13917db92496f66cb8ea7e86a4e98bd09ce979db
pchchv <ipchchv@gmail.com>
2024-04-11 18:06:20 +0300

    Implement a _buildLoadingIndicator

--- flutter_tutorial/inf_scroll_app/lib/main.dart
+++ flutter_tutorial/inf_scroll_app/lib/main.dart
@@ -1,1 +1,12 @@
 void main() {}
+
+class _InfiniteScrollingListViewState extends State<InfiniteScrollingListView> {
+  Widget _buildLoadingIndicator() {
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
