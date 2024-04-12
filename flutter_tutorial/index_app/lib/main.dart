64ba2e201ab6e2e8008254585e8d468c0d922e35
pchchv <ipchchv@gmail.com>
2024-04-12 11:51:56 +0300

    Implement _showItemDetails

--- flutter_tutorial/index_app/lib/main.dart
+++ flutter_tutorial/index_app/lib/main.dart
@@ -14,3 +14,27 @@ class IndexApp extends StatelessWidget {
     );
   }
 }
+
+class GridViewWithAlertDialog extends StatelessWidget {
+  const GridViewWithAlertDialog({super.key});
+
+  void _showItemDetails(BuildContext context, int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Item Details"),
+          content: Text("You tapped on item $index."),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text("Close"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
