c15639730ebc4f547355ebd0213d7e5631363d53
pchchv <ipchchv@gmail.com>
2024-04-12 11:50:14 +0300

    Implement an IndexApp

--- flutter_tutorial/index_app/lib/main.dart
+++ flutter_tutorial/index_app/lib/main.dart
@@ -1,1 +1,16 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const IndexApp());
+}
+
+class IndexApp extends StatelessWidget {
+  const IndexApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "GridView with AlertDialog",
+    );
+  }
+}
