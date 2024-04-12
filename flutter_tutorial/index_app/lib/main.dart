9bc1e01c87dd0755a5e9ced7922e9228a1eb95ac
pchchv <ipchchv@gmail.com>
2024-04-12 11:52:17 +0300

    Implement a GridViewWithAlertDialog

--- flutter_tutorial/index_app/lib/main.dart
+++ flutter_tutorial/index_app/lib/main.dart
@@ -11,12 +11,49 @@ class IndexApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "GridView with AlertDialog",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const GridViewWithAlertDialog(),
     );
   }
 }
 
 class GridViewWithAlertDialog extends StatelessWidget {
   const GridViewWithAlertDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("GridView with AlertDialog"),
+      ),
+      body: GridView.builder(
+        itemCount: 20,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 5,
+          mainAxisSpacing: 0.5,
+          crossAxisSpacing: 0.5,
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              _showItemDetails(context, index);
+            },
+            child: Container(
+              color: Colors.cyanAccent,
+              child: Center(
+                child: Text(
+                  "Item $index",
+                  style: const TextStyle(color: Colors.blueAccent),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
 
   void _showItemDetails(BuildContext context, int index) {
     showDialog(
