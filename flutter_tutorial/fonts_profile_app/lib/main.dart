2e8cf48c43831953fbf5adaf81f922577f71fc02
pchchv <ipchchv@gmail.com>
2024-04-16 15:20:25 +0300

    Implement a ProfileScreen

--- flutter_tutorial/fonts_profile_app/lib/main.dart
+++ flutter_tutorial/fonts_profile_app/lib/main.dart
@@ -1,1 +1,47 @@
 void main() {}
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Profile"),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage("assets/images/profile_photo.jpeg"), // photo asset
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Jack Pochechuev",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Moscow",
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "+79998887766",
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
