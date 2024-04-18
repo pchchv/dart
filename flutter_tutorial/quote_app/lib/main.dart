035ed8652a44d932d1cbd07b4a4b0458a9345d14
pchchv <ipchchv@gmail.com>
2024-04-18 10:31:19 +0300

    Implement a OnlineService for load JSON data

--- flutter_tutorial/quote_app/lib/main.dart
+++ flutter_tutorial/quote_app/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 class Quote {
   final String text;
   final String from;
@@ -12,6 +15,19 @@ class Quote {
       text: json['text'],
       from: json['from'],
     );
+  }
+}
+
+class OnlineService {
+  // Load and decode the JSON File
+  Future<Quote> loadQuote() async {
+    final response = await http.get(Uri.parse('https://jsonguide.technologychannel.org/quotes.json'));
+    if (response.statusCode == 200) {
+      final jsonResponse = json.decode(response.body);
+      return Quote.fromJSON(jsonResponse);
+    } else {
+      throw Exception('Failed to load data from server');
+    }
   }
 }
 
