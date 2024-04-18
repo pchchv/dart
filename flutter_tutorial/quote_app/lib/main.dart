08885db066c83ded2471520e8318266bdcd5301b
pchchv <ipchchv@gmail.com>
2024-04-18 11:43:39 +0300

    Update Quote class

--- flutter_tutorial/quote_app/lib/main.dart
+++ flutter_tutorial/quote_app/lib/main.dart
@@ -4,17 +4,17 @@ import 'package:http/http.dart' as http;
 
 class Quote {
   final String text;
-  final String from;
-  
+  final String author;
+
   Quote({
     required this.text,
-    required this.from
+    required this.author
   });
 
   factory Quote.fromJSON(Map<String, dynamic> json) {
     return Quote(
-      text: json['text'],
-      from: json['from'],
+      text: json['text'] as String,
+      author: json['from'] as String,
     );
   }
 }
