a1576234b51e95d3714ce0996ca03c2f0f7d72b3
pchchv <ipchchv@gmail.com>
2024-04-18 10:27:38 +0300

    Implement convertation from JSON to Person Object

--- flutter_tutorial/quote_app/lib/main.dart
+++ flutter_tutorial/quote_app/lib/main.dart
@@ -6,6 +6,13 @@ class Quote {
     required this.text,
     required this.from
   })
+
+  factory Quote.fromJSON(Map<String, dynamic> json) {
+    return Quote(
+      text: json['text'],
+      from: json['from'],
+    );
+  }
 }
 
 void main() {}
