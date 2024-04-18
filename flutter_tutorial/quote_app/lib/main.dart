4edbb0d4d1ecb47c2862d847b8b4ce2acf2102c2
pchchv <ipchchv@gmail.com>
2024-04-18 10:51:24 +0300

    Implement app logic

--- flutter_tutorial/quote_app/lib/main.dart
+++ flutter_tutorial/quote_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Quote {
@@ -38,4 +39,55 @@ void main() {
 class QuoteApp extends StatelessWidget {
   const QuoteApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = "Quote app";
+
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: FutureBuilder(
+          future: OnlineService().loadQuote(),
+          builder: (context, snapshot) {
+            Quote quote = snapshot.data as Quote;
+            return Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Card(
+                  elevation: 4.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          quote.text,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          quote.from,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
 }
