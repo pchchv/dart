cefeae22a3550a367db02b8cfdf380a4d41038ce
pchchv <ipchchv@gmail.com>
2024-04-19 11:26:48 +0300

    Implement convertation from JSON to Quote Object

--- flutter_tutorial/local_quote_app/lib/main.dart
+++ flutter_tutorial/local_quote_app/lib/main.dart
@@ -6,6 +6,13 @@ class Quote {
     required this.text,
     required this.author
   });
+
+  factory Quote.fromJSON(Map<String, dynamic> json) {
+    return Quote(
+      text: json['text'] as String,
+      author: json['from'] as String,
+    );
+  }
 }
 
 void main() {}
