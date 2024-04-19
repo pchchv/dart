ddff6510a7f09ad1b08aea162000ed5125dc5dc9
pchchv <ipchchv@gmail.com>
2024-04-19 11:55:24 +0300

    Implement a ScaleApp

--- flutter_tutorial/image_scale_app/lib/main.dart
+++ flutter_tutorial/image_scale_app/lib/main.dart
@@ -1,1 +1,20 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ScaleApp());
+}
+
+class ScaleApp extends StatelessWidget {
+  const ScaleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ScaleHomePage(),
+    );
+  }
+}
+
+class ScaleHomePage extends StatelessWidget {
+  const ScaleHomePage({super.key});
+}
