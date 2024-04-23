176cf44a5d8b228b30a51c9eb4ecb9dd1cef6dea
pchchv <ipchchv@gmail.com>
2024-04-23 12:16:58 +0300

    Implement an ImageApp

--- flutter_tutorial/web_image_app/lib/main.dart
+++ flutter_tutorial/web_image_app/lib/main.dart
@@ -1,1 +1,19 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ImageApp());
+}
+
+class ImageApp extends StatelessWidget {
+  const ImageApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Image Viewer",
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      )
+    );
+  }
+}
