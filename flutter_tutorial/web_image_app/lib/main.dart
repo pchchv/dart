2cf1fff5f9664abcde556ab0105ccae1ca43638b
pchchv <ipchchv@gmail.com>
2024-04-23 12:22:30 +0300

    Implement an ImageHomePage

--- flutter_tutorial/web_image_app/lib/main.dart
+++ flutter_tutorial/web_image_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const ImageApp());
@@ -13,9 +14,17 @@ class ImageApp extends StatelessWidget {
       title: "Image Viewer",
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
-      )
+      ),
+      home: const ImageHomePage(),
     );
   }
+}
+
+class ImageHomePage extends StatefulWidget {
+  const ImageHomePage({super.key});
+
+  @override
+  ImageyHomePageState createState() => ImageyHomePageState();
 }
 
 class ImageyHomePageState extends State<ImageHomePage> {
