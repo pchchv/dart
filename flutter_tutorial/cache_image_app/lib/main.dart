51c20a77377cf43359c4301337deeecea9b72a4f
pchchv <ipchchv@gmail.com>
2024-04-24 10:54:28 +0300

    Implement CacheApp

--- flutter_tutorial/cache_image_app/lib/main.dart
+++ flutter_tutorial/cache_image_app/lib/main.dart
@@ -3,3 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const CacheApp());
 }
+
+class CacheApp extends StatelessWidget {
+  const CacheApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Cached Image App",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      )
+    );
+  }
+}
