ca0728f2ecc1a42a5adfbaed486c5e6ad8c8a8c2
pchchv <ipchchv@gmail.com>
2024-04-24 10:55:39 +0300

    Implement the CachedImage

--- flutter_tutorial/cache_image_app/lib/main.dart
+++ flutter_tutorial/cache_image_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() {
   runApp(const CacheApp());
@@ -14,7 +15,25 @@ class CacheApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-      )
+      ),
+      home: const CachedImage(),
+    );
+  }
+}
+
+class CachedImage extends StatelessWidget {
+  const CachedImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CachedNetworkImage(
+          imageUrl: "https://picsum.photos/500?image=100",
+          placeholder: (context, url) => const CircularProgressIndicator(),
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+        ),
+      ),
     );
   }
 }
