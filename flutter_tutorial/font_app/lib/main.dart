bd708aa6e4c32e184c4ce629acc67e39c74325fe
pchchv <ipchchv@gmail.com>
2024-04-24 11:12:34 +0300

    Implement a HomeScreen

--- flutter_tutorial/font_app/lib/main.dart
+++ flutter_tutorial/font_app/lib/main.dart
@@ -3,3 +3,22 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const FontApp());
 }
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Custom Font App", style: TextStyle(fontSize: 24, fontFamily: "JetBrainsMono")),
+      ),
+      body: const Center(
+        child: Text(
+          "Hello, world!",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
