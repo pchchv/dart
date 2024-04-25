abf498acac9dec26dba0c207bea622b62b280d70
pchchv <ipchchv@gmail.com>
2024-04-25 08:38:49 +0300

    Implement a JSONApp

--- flutter_tutorial/json_app/lib/main.dart
+++ flutter_tutorial/json_app/lib/main.dart
@@ -6,4 +6,15 @@ void main() {
 
 class JSONApp extends StatelessWidget {
   const JSONApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "JSON Reader",
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      )
+    );
+  }
 }
