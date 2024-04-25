9ac9b4906f3131f6a2f4bd3946bd3976a0d9ceea
pchchv <ipchchv@gmail.com>
2024-04-25 08:41:26 +0300

    Implement a data loading

--- flutter_tutorial/json_app/lib/main.dart
+++ flutter_tutorial/json_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 void main() {
   runApp(const JSONApp());
@@ -30,4 +31,18 @@ class JsonReader extends StatefulWidget {
 
 class _JsonReaderState extends State<JsonReader> {
   List<dynamic> _data = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadJsonData();
+  }
+
+  Future<void> _loadJsonData() async {
+    String jsonString = await DefaultAssetBundle.of(context)
+        .loadString('assets/data.json');
+    setState(() {
+      _data = json.decode(jsonString);
+    });
+  }
 }
