25b4e09d74f534c2899d4f688642c68925a81245
pchchv <ipchchv@gmail.com>
2024-04-26 10:46:43 +0300

    Implement a QuotesApp

--- flutter_tutorial/quotes_app/lib/main.dart
+++ flutter_tutorial/quotes_app/lib/main.dart
@@ -3,3 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const QuotesApp());
 }
+
+class QuotesApp extends StatelessWidget {
+  const QuotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quotes App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      )
+    );
+  }
+}
