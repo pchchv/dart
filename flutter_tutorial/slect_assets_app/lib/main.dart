5463b5b880d5e6b5c8c940bd4005d3c086e04ceb
pchchv <ipchchv@gmail.com>
2024-05-02 11:51:12 +0300

    Implement a SelectsAssetsApp

--- flutter_tutorial/slect_assets_app/lib/main.dart
+++ flutter_tutorial/slect_assets_app/lib/main.dart
@@ -2,6 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() {}
 
+class SelectsAssetsApp extends StatefulWidget {
+  const SelectsAssetsApp({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SelectsAssetsAppState createState() => _SelectsAssetsAppState();
+}
+
 class _SelectsAssetsAppState extends State<SelectsAssetsApp> {
   bool useSet1 = true;
 
