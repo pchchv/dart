12027c9e2eb48db46b965bc56f5212621d7c9b66
pchchv <ipchchv@gmail.com>
2024-05-02 11:50:34 +0300

    Implement a _SelectsAssetsAppState

--- flutter_tutorial/slect_assets_app/lib/main.dart
+++ flutter_tutorial/slect_assets_app/lib/main.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
+
 void main() {}
+
+class _SelectsAssetsAppState extends State<SelectsAssetsApp> {
+  bool useSet1 = true;
+
+  void _toggleAssets() {
+    setState(() {
+      useSet1 = !useSet1;
+    });
+  }
+}
