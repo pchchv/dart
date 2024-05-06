20df28d1e2a37ef70850c3d89a2ab0fbc02fb24d
pchchv <ipchchv@gmail.com>
2024-05-06 12:18:19 +0300

    Implement an AccountingApp

--- flutter_tutorial/snackbar_app/lib/main.dart
+++ flutter_tutorial/snackbar_app/lib/main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+
 void main() {}
+
+class AccountingApp extends StatelessWidget {
+  const AccountingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      )
+    );
+  }
+}
