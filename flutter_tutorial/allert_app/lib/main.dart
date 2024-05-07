375f0c756230054aedb1abf0ec6ee1d14c5855a9
pchchv <ipchchv@gmail.com>
2024-05-07 11:46:27 +0300

    Add scaffold

--- flutter_tutorial/allert_app/lib/main.dart
+++ flutter_tutorial/allert_app/lib/main.dart
@@ -17,4 +17,13 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gym App'),
+      ),
+    );
+  }
 }
