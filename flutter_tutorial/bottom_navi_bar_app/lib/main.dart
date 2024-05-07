7487c3f9844c23ee7f4b9e778f058ad9a597d1a3
pchchv <ipchchv@gmail.com>
2024-05-07 11:19:50 +0300

    Implement a MyApp

--- flutter_tutorial/bottom_navi_bar_app/lib/main.dart
+++ flutter_tutorial/bottom_navi_bar_app/lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
+
 void main() {}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gym App',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      )
+    );
+  }
+}
