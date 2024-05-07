3217b1fcabc703f473eeeffd166ffe3b260b45b1
pchchv <ipchchv@gmail.com>
2024-05-07 11:47:06 +0300

    Implement buttob with allert dialog

--- flutter_tutorial/allert_app/lib/main.dart
+++ flutter_tutorial/allert_app/lib/main.dart
@@ -24,6 +24,32 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Gym App'),
       ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Show the alert dialog
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text('Success'),
+                  content: const Text('Account created successfully'),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                        // Close the dialog
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('OK'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          child: const Text('Register'),
+        ),
+      ),
     );
   }
 }
