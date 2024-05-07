5bd6b2d6a032fe8c72984fb3655bf390932eeb70
pchchv <ipchchv@gmail.com>
2024-05-07 11:23:16 +0300

    Implement a MyHomePage

--- flutter_tutorial/bottom_navi_bar_app/lib/main.dart
+++ flutter_tutorial/bottom_navi_bar_app/lib/main.dart
@@ -13,7 +13,19 @@ class MyApp extends StatelessWidget {
       title: 'Gym App',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
-      )
+      ),
+      home: const MyHomePage(),
     );
   }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
 }
