459b83e17bd140e3c94627ad45911b23286a8417
pchchv <ipchchv@gmail.com>
2024-05-08 11:55:54 +0300

    Implement a Product class

--- flutter_tutorial/data_table_app/lib/main.dart
+++ flutter_tutorial/data_table_app/lib/main.dart
@@ -18,3 +18,11 @@ class DataApp extends StatelessWidget {
     );
   }
 }
+
+class Product {
+  final String name;
+  final double price;
+  final int stock;
+
+  Product({required this.name, required this.price, required this.stock});
+}
