bcc12f243b6496a203389ae61cc61d4c94bcd95b
pchchv <ipchchv@gmail.com>
2024-05-08 11:53:49 +0300

    Implement a DataApp

--- flutter_tutorial/data_table_app/lib/main.dart
+++ flutter_tutorial/data_table_app/lib/main.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+
 void main() {}
+
+class DataApp extends StatelessWidget {
+  const DataApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Product DataTable'),
+        )
+      ),
+    );
+  }
+}
