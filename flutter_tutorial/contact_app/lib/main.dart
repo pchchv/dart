fb94b61aa26774bfed899b74eed2f3eca43f396c
pchchv <ipchchv@gmail.com>
2024-05-10 11:27:50 +0300

    Implement a ContactApp

--- flutter_tutorial/contact_app/lib/main.dart
+++ flutter_tutorial/contact_app/lib/main.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+
 void main() {}
+
+class ContactApp extends StatelessWidget {
+  const ContactApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Custom Form',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      )
+    );
+  }
+}
