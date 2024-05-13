956780ae0ae16f6b54ed9d355b62ac5e433b81d5
pchchv <ipchchv@gmail.com>
2024-05-13 12:14:26 +0300

    Implement a _sort func

--- flutter_tutorial/products_app/lib/main.dart
+++ flutter_tutorial/products_app/lib/main.dart
@@ -36,11 +36,41 @@ class ProductTablePage extends StatefulWidget {
 }
 
 class _ProductTablePageState extends State<ProductTablePage> {
+  String _sortColumn = 'name';
+  bool _sortAscending = true;
   final List<Product> _products = [
     Product(name: 'Product A', price: 15.55, quantity: 5),
     Product(name: 'Product B', price: 15.00, quantity: 3),
     Product(name: 'Product C', price: 20.00, quantity: 7),
   ];
+
+  void _sort(String column) {
+    if (_sortColumn == column) {
+      setState(() {
+        _sortAscending = !_sortAscending;
+      });
+    } else {
+      setState(() {
+        _sortColumn = column;
+        _sortAscending = true;
+      });
+    }
+    _products.sort((a, b) {
+      if (column == 'name') {
+        return _sortAscending
+            ? a.name.compareTo(b.name)
+            : b.name.compareTo(a.name);
+      } else if (column == 'price') {
+        return _sortAscending
+            ? a.price.compareTo(b.price)
+            : b.price.compareTo(a.price);
+      } else {
+        return _sortAscending
+            ? a.quantity.compareTo(b.quantity)
+            : b.quantity.compareTo(a.quantity);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
