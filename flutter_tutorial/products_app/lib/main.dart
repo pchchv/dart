b2001fa033bb38158fa12630ad8d6e80db569a77
pchchv <ipchchv@gmail.com>
2024-05-13 12:07:36 +0300

    Implement a ProductsApp

--- flutter_tutorial/products_app/lib/main.dart
+++ flutter_tutorial/products_app/lib/main.dart
@@ -1,7 +1,22 @@
+import 'package:flutter/material.dart';
+
 class Product {
   final String name;
   final double price;
   final int quantity;
 
   Product({required this.name, required this.price, required this.quantity});
+}
+class ProductsApp extends StatelessWidget {
+  const ProductsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Product Table',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
