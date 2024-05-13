960c7ac740e2593368a8e049ac57113bab75060d
pchchv <ipchchv@gmail.com>
2024-05-13 12:06:44 +0300

    Add Product class

--- flutter_tutorial/products_app/lib/main.dart
+++ flutter_tutorial/products_app/lib/main.dart
@@ -1,1 +1,7 @@
-void main() {}
+class Product {
+  final String name;
+  final double price;
+  final int quantity;
+
+  Product({required this.name, required this.price, required this.quantity});
+}
