4ca6cc1c74b0ee5227a987c574864fcd71f2ff77
pchchv <ipchchv@gmail.com>
2024-05-13 11:26:37 +0300

    Implement a ButtonsApp

--- flutter_tutorial/buttons_app/lib/main.dart
+++ flutter_tutorial/buttons_app/lib/main.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
+
 void main() {}
+
+class ButtonsApp extends StatelessWidget {
+  const ButtonsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Button Demo',
+    );
+  }
+}
