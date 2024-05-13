d152197ff5b3c3cd1eb8e72264651d5552398d5b
pchchv <ipchchv@gmail.com>
2024-05-13 11:27:48 +0300

    Implement a ButtonScreen

--- flutter_tutorial/buttons_app/lib/main.dart
+++ flutter_tutorial/buttons_app/lib/main.dart
@@ -9,6 +9,20 @@ class ButtonsApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Button Demo',
+      home: ButtonScreen(),
+    );
+  }
+}
+
+class ButtonScreen extends StatelessWidget {
+  const ButtonScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Button Demo'),
+      ),
     );
   }
 }
