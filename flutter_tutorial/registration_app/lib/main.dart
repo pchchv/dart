a52f53bbec1f362f0f434445e65d754ea5076c81
pchchv <ipchchv@gmail.com>
2024-05-14 11:05:29 +0300

    Implement a RegistrationApp

--- flutter_tutorial/registration_app/lib/main.dart
+++ flutter_tutorial/registration_app/lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
+
 void main() {}
+
+class RegistrationApp extends StatelessWidget {
+  const RegistrationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dynamic Form',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      )
+    );
+  }
+}
