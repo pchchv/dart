e4be256fe2e38cf7a4a7a470b8f7d83af42f8be7
pchchv <ipchchv@gmail.com>
2024-05-15 10:34:22 +0300

    Implement a MultiStepApp

--- flutter_tutorial/multi_step_app/lib/main.dart
+++ flutter_tutorial/multi_step_app/lib/main.dart
@@ -1,2 +1,18 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class MultiStepApp extends StatelessWidget {
+  const MultiStepApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Multi-step Form',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
