aab75cbc3eec12fdaad8bc0873939b51a086942c
pchchv <ipchchv@gmail.com>
2024-05-15 10:21:41 +0300

    Add ResponsiveLayout

--- flutter_tutorial/list_and_table_app/lib/main.dart
+++ flutter_tutorial/list_and_table_app/lib/main.dart
@@ -14,6 +14,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: const ResponsiveLayout(),
+    );
+  }
+}
+
+class ResponsiveLayout extends StatelessWidget {
+  const ResponsiveLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Responsive Layout Example'),
+      ),
     );
   }
 }
