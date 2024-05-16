1a427ac0d69175447841b51126da53f3d266a906
pchchv <ipchchv@gmail.com>
2024-05-16 22:34:58 +0300

    Implement a ButtonApp

--- flutter_tutorial/custom_button_app/lib/main.dart
+++ flutter_tutorial/custom_button_app/lib/main.dart
@@ -1,2 +1,25 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class ButtonApp extends StatelessWidget {
+  const ButtonApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: CustomButton(
+          text: 'Press Me',
+          onPressed: () {
+            // ignore: avoid_print
+            print('Button Pressed!');
+          },
+        ),
+      ),
+    ),
+  );
+  }
 }
