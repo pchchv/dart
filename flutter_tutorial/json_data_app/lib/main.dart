fc7badd0790c5eb35b63f2c435e98d622aa390ff
pchchv <ipchchv@gmail.com>
2024-05-16 22:46:00 +0300

    Implement a DataApp

--- flutter_tutorial/json_data_app/lib/main.dart
+++ flutter_tutorial/json_data_app/lib/main.dart
@@ -1,2 +1,18 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class DataApp extends StatelessWidget {
+  const DataApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'User Data Table',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
