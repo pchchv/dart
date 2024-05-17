213c4d7647e26284a26635253c0712eafe734e29
pchchv <ipchchv@gmail.com>
2024-05-17 15:29:20 +0300

    Implement a SignApp

--- flutter_tutorial/sign_in_app/lib/main.dart
+++ flutter_tutorial/sign_in_app/lib/main.dart
@@ -1,2 +1,18 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class SignApp extends StatelessWidget {
+  const SignApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sign In/Sign Up Animation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
