eb8b115555cfe1f60ded00f16cb3f06c055271e7
pchchv <ipchchv@gmail.com>
2024-05-20 19:14:57 +0300

    Implement a ButtonApp

--- flutter_tutorial/material_button_app/lib/main.dart
+++ flutter_tutorial/material_button_app/lib/main.dart
@@ -1,2 +1,18 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class ButtonApp extends StatelessWidget {
+  const ButtonApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Button App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
