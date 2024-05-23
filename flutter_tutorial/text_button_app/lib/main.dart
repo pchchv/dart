5d5096403f5abc2cdd3faf7eba59d958e29f8a36
pchchv <ipchchv@gmail.com>
2024-05-23 12:14:30 +0300

    Create an empty app

--- flutter_tutorial/text_button_app/lib/main.dart
+++ flutter_tutorial/text_button_app/lib/main.dart
@@ -1,2 +1,37 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const ButtonApp());
+}
+
+class ButtonApp extends StatelessWidget {
+  const ButtonApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('TextButton Color Change'),
+        ),
+        body: const Center(
+          child: ColorChangingTextButton(),
+        ),
+      ),
+    );
+  }
+}
+
+class ColorChangingTextButton extends StatefulWidget {
+  const ColorChangingTextButton({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ColorChangingTextButtonState createState() => _ColorChangingTextButtonState();
+}
+
+class _ColorChangingTextButtonState extends State<ColorChangingTextButton> {
+  @override
+  Widget build(BuildContext context) {
+  }
 }
