b67714be09f84fcbad25e7e1321b8049b2b3c290
pchchv <ipchchv@gmail.com>
2024-05-23 11:55:24 +0300

    Create an empty app

--- flutter_tutorial/icon_button_app/lib/main.dart
+++ flutter_tutorial/icon_button_app/lib/main.dart
@@ -1,2 +1,38 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const ButtonApp());
+}
+
+class ButtonApp extends StatelessWidget {
+  const ButtonApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('IconButton Color Change'),
+        ),
+        body: const Center(
+          child: ColorChangingIconButton(),
+        ),
+      ),
+    );
+  }
+}
+
+class ColorChangingIconButton extends StatefulWidget {
+  const ColorChangingIconButton({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ColorChangingIconButtonState createState() => _ColorChangingIconButtonState();
+}
+
+class _ColorChangingIconButtonState extends State<ColorChangingIconButton> {
+
+  @override
+  Widget build(BuildContext context) {
+  }
 }
