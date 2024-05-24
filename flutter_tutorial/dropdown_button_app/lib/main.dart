d19f6c1f2eb9e5628a22b63bbb5bc4ea573ac8ef
pchchv <ipchchv@gmail.com>
2024-05-24 12:04:13 +0300

    Create empty app

--- flutter_tutorial/dropdown_button_app/lib/main.dart
+++ flutter_tutorial/dropdown_button_app/lib/main.dart
@@ -1,2 +1,40 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const ButtonApp());
+}
+
+class ButtonApp extends StatelessWidget {
+  const ButtonApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Days app'),
+        ),
+        body: const Center(
+          child: DaySelector(),
+        ),
+      ),
+    );
+  }
+}
+
+class DaySelector extends StatefulWidget {
+  const DaySelector({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _DaySelectorState createState() => _DaySelectorState();
+}
+
+class _DaySelectorState extends State<DaySelector> {
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center
+    );
+  }
 }
