ebb54da72abe6608d4109dbd742614e7f559e42b
pchchv <ipchchv@gmail.com>
2024-05-24 12:31:58 +0300

    Create empty app

--- flutter_tutorial/popup_menu_button_app/lib/main.dart
+++ flutter_tutorial/popup_menu_button_app/lib/main.dart
@@ -1,2 +1,20 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const ButtonApp());
+}
+
+class ButtonApp extends StatefulWidget {
+  const ButtonApp({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ButtonAppState createState() => _ButtonAppState();
+}
+
+class _ButtonAppState extends State<ButtonApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
 }
