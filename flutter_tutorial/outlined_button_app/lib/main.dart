cd21bc91381d60281f08fff21f4ba7a2b5dcc9e5
pchchv <ipchchv@gmail.com>
2024-05-24 11:59:16 +0300

    Refactoring

--- flutter_tutorial/outlined_button_app/lib/main.dart
+++ flutter_tutorial/outlined_button_app/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ButtonApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ButtonApp extends StatelessWidget {
+  const ButtonApp({super.key});
 
   @override
   Widget build(BuildContext context) {
