a4c91910a3ad8da8f73e078db8825949b34cf047
pchchv <ipchchv@gmail.com>
2024-05-30 10:59:54 +0300

    Create an empty app

--- flutter_tutorial/dropdown_button_q_app/lib/main.dart
+++ flutter_tutorial/dropdown_button_q_app/lib/main.dart
@@ -1,2 +1,38 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const ButtonApp());
+}
+
+class ButtonApp extends StatelessWidget {
+  const ButtonApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('DropdownButton App'),
+        ),
+        body: const Center(
+          child: DropdownButtonExample(),
+        ),
+      ),
+    );
+  }
+}
+
+class DropdownButtonExample extends StatefulWidget {
+  const DropdownButtonExample({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _DropdownButtonExampleState createState() => _DropdownButtonExampleState();
+}
+
+class _DropdownButtonExampleState extends State<DropdownButtonExample> {
+  @override
+  Widget build(BuildContext context) {
+    return {};
+  }
 }
