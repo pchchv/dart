ac5d8d389a8ec649eab22bfefb30480c0fa9d18f
pchchv <ipchchv@gmail.com>
2024-05-30 11:03:59 +0300

    Create an empty app

--- flutter_tutorial/popup_menu_button_q_app/lib/main.dart
+++ flutter_tutorial/popup_menu_button_q_app/lib/main.dart
@@ -1,2 +1,46 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const ButtonApp());
+}
+
+class ButtonApp extends StatelessWidget {
+  const ButtonApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('PopupMenuButton App'),
+        ),
+        body: const Center(
+          child: PopupMenuButtonExample(),
+        ),
+      ),
+    );
+  }
+}
+
+class PopupMenuButtonExample extends StatefulWidget {
+  const PopupMenuButtonExample({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _PopupMenuButtonExampleState createState() => _PopupMenuButtonExampleState();
+}
+
+class _PopupMenuButtonExampleState extends State<PopupMenuButtonExample> {
+  void _handleMenuItemClick(String value) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Selected: $value'),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ();
+  }
 }
