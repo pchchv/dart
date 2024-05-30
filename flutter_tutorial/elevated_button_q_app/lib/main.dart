5caa2207b6c35d639e5d95db4896db785417c613
pchchv <ipchchv@gmail.com>
2024-05-30 10:41:00 +0300

    Create an empty app

--- flutter_tutorial/elevated_button_q_app/lib/main.dart
+++ flutter_tutorial/elevated_button_q_app/lib/main.dart
@@ -1,2 +1,39 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const ButtonApp());
+}
+
+class ButtonApp extends StatelessWidget {
+  const ButtonApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ButtonHomePage(),
+    );
+  }
+}
+
+class ButtonHomePage extends StatefulWidget {
+  const ButtonHomePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ButtonHomePageState createState() => _ButtonHomePageState();
+}
+
+class _ButtonHomePageState extends State<ButtonHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ElevatedButton Example'),
+      ),
+    );
+  }
 }
