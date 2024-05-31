aea4e19c17b6258c1199d832f99fe8fa7c1cae1f
pchchv <ipchchv@gmail.com>
2024-05-31 11:04:35 +0300

    Create an empty app

--- flutter_tutorial/checkbox_app/lib/main.dart
+++ flutter_tutorial/checkbox_app/lib/main.dart
@@ -1,2 +1,36 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const CheckBoxApp());
+}
+
+class CheckBoxApp extends StatelessWidget {
+  const CheckBoxApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Select languages'),
+        ),
+        body: const LanguagesScreen(),
+      ),
+    );
+  }
+}
+
+class LanguagesScreen extends StatefulWidget {
+  const LanguagesScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LanguagesScreenState createState() => _LanguagesScreenState();
+}
+
+class _LanguagesScreenState extends State<LanguagesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView();
+  }
 }
