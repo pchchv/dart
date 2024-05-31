96ab86ed5bc8941ad2849f7aaac2a06e042d8808
pchchv <ipchchv@gmail.com>
2024-05-31 10:50:47 +0300

    Create an empty app

--- flutter_tutorial/text_form_field_app/lib/main.dart
+++ flutter_tutorial/text_form_field_app/lib/main.dart
@@ -1,2 +1,36 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const FormApp());
+}
+
+class FormApp extends StatelessWidget {
+  const FormApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Feedback Form'),
+        ),
+        body: const FeedbackForm(),
+      ),
+    );
+  }
+}
+
+class FeedbackForm extends StatefulWidget {
+  const FeedbackForm({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _FeedbackFormState createState() => _FeedbackFormState();
+}
+
+class _FeedbackFormState extends State<FeedbackForm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(padding: null);
+  }
 }
