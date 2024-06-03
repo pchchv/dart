6a1364da956c8f1510c187b6396eb75d84b22ed0
pchchv <ipchchv@gmail.com>
2024-06-03 10:32:56 +0300

    Add questions list

--- flutter_tutorial/quiz_app/lib/quiz_data.dart
+++ flutter_tutorial/quiz_app/lib/quiz_data.dart
@@ -9,3 +9,18 @@ class Question {
     required this.correctAnswerIndex,
   });
 }
+
+List<Question> getQuestions() {
+  return [
+    Question(
+      questionText: "What is the capital of France?",
+      options: ["Berlin", "Madrid", "Paris", "Lisbon"],
+      correctAnswerIndex: 2,
+    ),
+    Question(
+      questionText: "Who wrote 'Hamlet'?",
+      options: ["Charles Dickens", "William Shakespeare", "Mark Twain", "Jane Austen"],
+      correctAnswerIndex: 1,
+    ),
+  ];
+}
