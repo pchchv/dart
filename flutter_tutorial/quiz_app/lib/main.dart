ff8757fa68ade61bab09a84357e0d5572e734d19
pchchv <ipchchv@gmail.com>
2024-06-03 10:28:08 +0300

    Implement a QuizApp

--- flutter_tutorial/quiz_app/lib/main.dart
+++ flutter_tutorial/quiz_app/lib/main.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const QuizApp());
+}
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
