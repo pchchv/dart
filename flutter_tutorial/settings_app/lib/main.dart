49022ce1e65883979550e11e92761adf470bbecd
pchchv <ipchchv@gmail.com>
2024-06-04 15:40:42 +0300

    Implement a SettingsApp

--- flutter_tutorial/settings_app/lib/main.dart
+++ flutter_tutorial/settings_app/lib/main.dart
@@ -1,2 +1,15 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class SettingsApp extends StatefulWidget {
+  const SettingsApp({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SettingsAppState createState() => _SettingsAppState();
+}
+
+class _SettingsAppState extends State<SettingsApp> {
 }
