7ed1b20f198bef4f81610340faf4ec771868f66e
pchchv <ipchchv@gmail.com>
2024-06-04 15:57:07 +0300

    Implement an AgeCalculatorApp

--- flutter_tutorial/age_app/lib/main.dart
+++ flutter_tutorial/age_app/lib/main.dart
@@ -1,2 +1,18 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class AgeCalculatorApp extends StatelessWidget {
+  const AgeCalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Age Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
