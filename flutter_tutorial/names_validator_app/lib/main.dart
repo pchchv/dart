90f9e6a9cfa06fe5aa052913511ac2523e994cfa
pchchv <ipchchv@gmail.com>
2024-06-06 12:18:41 +0300

    Implement a NamesApp

--- flutter_tutorial/names_validator_app/lib/main.dart
+++ flutter_tutorial/names_validator_app/lib/main.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const NamesApp());
+}
+
+class NamesApp extends StatelessWidget {
+  const NamesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Form Validation Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
