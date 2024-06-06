2562378db73393512ba85b2468c0de5c2344ac2e
pchchv <ipchchv@gmail.com>
2024-06-06 12:28:36 +0300

    Implement a FormApp

--- flutter_tutorial/email_pass_app/lib/main.dart
+++ flutter_tutorial/email_pass_app/lib/main.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const FormApp());
+}
+
+class FormApp extends StatelessWidget {
+  const FormApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Form Validation Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
