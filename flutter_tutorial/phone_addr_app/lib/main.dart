819b97b5d1b133d9bbac34058f8009b0d6e66866
pchchv <ipchchv@gmail.com>
2024-06-07 12:46:37 +0300

    Implement a FormApp

--- flutter_tutorial/phone_addr_app/lib/main.dart
+++ flutter_tutorial/phone_addr_app/lib/main.dart
@@ -1,2 +1,18 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class FormApp extends StatelessWidget {
+  const FormApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Form Validation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
