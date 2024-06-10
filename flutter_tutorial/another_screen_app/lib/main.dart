71b3debea89ecadcad9363799ca676ee9e820033
pchchv <ipchchv@gmail.com>
2024-06-10 14:16:38 +0300

    Create an empty homescreen

--- flutter_tutorial/another_screen_app/lib/main.dart
+++ flutter_tutorial/another_screen_app/lib/main.dart
@@ -12,6 +12,20 @@ class ScreenApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Navigation App',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Screen"),
+      ),
     );
   }
 }
