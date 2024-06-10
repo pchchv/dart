eb0a827c9b888226b0ab3128d17c3df8a454b3a9
pchchv <ipchchv@gmail.com>
2024-06-10 14:14:06 +0300

    Implement a ScreenApp

--- flutter_tutorial/another_screen_app/lib/main.dart
+++ flutter_tutorial/another_screen_app/lib/main.dart
@@ -1,2 +1,16 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class ScreenApp extends StatelessWidget {
+  const ScreenApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Navigation App',
+    );
+  }
 }
