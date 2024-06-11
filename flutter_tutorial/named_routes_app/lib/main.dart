f14f04109c96e8a71b77aeedc41a0dbb06190933
pchchv <ipchchv@gmail.com>
2024-06-11 13:53:34 +0300

    Implement a ScreenApp

--- flutter_tutorial/named_routes_app/lib/main.dart
+++ flutter_tutorial/named_routes_app/lib/main.dart
@@ -1,2 +1,18 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class ScreenApp extends StatelessWidget {
+  const ScreenApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Shop App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
