bf4d49483f8261da44ca00cd67c174560a2b711e
pchchv <ipchchv@gmail.com>
2024-06-11 13:55:12 +0300

    Create an empty home screen

--- flutter_tutorial/named_routes_app/lib/main.dart
+++ flutter_tutorial/named_routes_app/lib/main.dart
@@ -17,3 +17,18 @@ class ScreenApp extends StatelessWidget {
     );
   }
 }
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: const Center(
+      ),
+    );
+  }
+}
