b6ee50c93666b2726c60371d664ae1d4fd59c56a
pchchv <ipchchv@gmail.com>
2024-06-11 14:12:52 +0300

    Implement a HomeScreen and _HomeScreenState

--- flutter_tutorial/color_screen_app/lib/main.dart
+++ flutter_tutorial/color_screen_app/lib/main.dart
@@ -17,3 +17,24 @@ class ColorScreenApp extends StatelessWidget {
     );
   }
 }
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: Container(
+      ),
+    );
+  }
+}
