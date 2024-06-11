1f28be56a41dda1a9ace5a7e010651d7e3518fc4
pchchv <ipchchv@gmail.com>
2024-06-11 14:11:29 +0300

    Implement a ColorScreenApp

--- flutter_tutorial/color_screen_app/lib/main.dart
+++ flutter_tutorial/color_screen_app/lib/main.dart
@@ -1,2 +1,18 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class ColorScreenApp extends StatelessWidget {
+  const ColorScreenApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Color Selection App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
