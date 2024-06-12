77d693dfac950e09cccdc57168cf49d7f5306e3a
pchchv <ipchchv@gmail.com>
2024-06-12 09:59:13 +0300

    Implement a HomeScreen

--- flutter_tutorial/page_route_app/lib/main.dart
+++ flutter_tutorial/page_route_app/lib/main.dart
@@ -11,6 +11,24 @@ class PagesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Navigation App',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Column(
+        ),
+      ),
     );
   }
 }
