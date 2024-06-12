5835320b2b96f50d3cf7e26324bf5a0b7d4da598
pchchv <ipchchv@gmail.com>
2024-06-12 09:57:22 +0300

    Implement a PagesApp

--- flutter_tutorial/page_route_app/lib/main.dart
+++ flutter_tutorial/page_route_app/lib/main.dart
@@ -1,2 +1,15 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class PagesApp extends StatelessWidget {
+  const PagesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Navigation App',
+    );
+  }
 }
