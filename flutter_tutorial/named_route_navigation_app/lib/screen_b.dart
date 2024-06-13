63d6a4ed8ff83065ffaa90a4f51dd64daa189cee
pchchv <ipchchv@gmail.com>
2024-06-13 12:52:09 +0300

    Implement a ScreenB

---
+++ flutter_tutorial/named_route_navigation_app/lib/screen_b.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ScreenB extends StatelessWidget {
+  const ScreenB({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Screen B'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Back to Home'),
+        ),
+      ),
+    );
+  }
+}
