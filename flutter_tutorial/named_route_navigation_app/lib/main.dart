cc74e453979094fec0c224ef69637f9cb93dfeeb
pchchv <ipchchv@gmail.com>
2024-06-13 12:48:07 +0300

    Implement a NavigationApp

--- flutter_tutorial/named_route_navigation_app/lib/main.dart
+++ flutter_tutorial/named_route_navigation_app/lib/main.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const NavigationApp());
+}
+
+class NavigationApp extends StatelessWidget {
+  const NavigationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Named Route Navigation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
