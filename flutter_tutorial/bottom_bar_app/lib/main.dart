b17ab5157f6506610594d4306ab5e9f913adc817
pchchv <ipchchv@gmail.com>
2024-06-13 12:36:16 +0300

    Implement a BarApp

--- flutter_tutorial/bottom_bar_app/lib/main.dart
+++ flutter_tutorial/bottom_bar_app/lib/main.dart
@@ -1,2 +1,16 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class BarApp extends StatelessWidget {
+  const BarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Bottom Navigation Bar',
+      home: HomeScreen(),
+    );
+  }
 }
