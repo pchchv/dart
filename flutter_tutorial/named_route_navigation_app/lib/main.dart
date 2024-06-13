8a56d16bd89bd9d2da4e122b3c3183073423ad84
pchchv <ipchchv@gmail.com>
2024-06-13 13:01:42 +0300

    Add routes

--- flutter_tutorial/named_route_navigation_app/lib/main.dart
+++ flutter_tutorial/named_route_navigation_app/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
+import 'screen_a.dart';
+import 'screen_b.dart';
 
 void main() {
   runApp(const NavigationApp());
@@ -14,6 +17,12 @@ class NavigationApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomeScreen(),
+        '/screenA': (context) => const ScreenA(),
+        '/screenB': (context) => const ScreenB(),
+      },
     );
   }
 }
