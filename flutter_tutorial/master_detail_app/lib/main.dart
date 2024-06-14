4788842de3f8d78fc1f80f6e30796336fbd12599
pchchv <ipchchv@gmail.com>
2024-06-14 09:13:02 +0300

    Implement an InterfaceApp

--- flutter_tutorial/master_detail_app/lib/main.dart
+++ flutter_tutorial/master_detail_app/lib/main.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const InterfaceApp());
+}
+
+class InterfaceApp extends StatelessWidget {
+  const InterfaceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Master-Detail App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
