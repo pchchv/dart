7855aedf206a64366da2b58ec4c39076b59f5673
pchchv <ipchchv@gmail.com>
2024-06-14 08:57:46 +0300

    Implement a PagesApp

--- flutter_tutorial/multi_page_form_app/lib/main.dart
+++ flutter_tutorial/multi_page_form_app/lib/main.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const PagesApp());
+}
+
+class PagesApp extends StatelessWidget {
+  const PagesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Multi-Page Form',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
