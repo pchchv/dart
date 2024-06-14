48fd1b6628e112c43044c70b96016ff5ef14ecf2
pchchv <ipchchv@gmail.com>
2024-06-14 09:03:36 +0300

    Implement a Step1

--- flutter_tutorial/multi_page_form_app/lib/step1.dart
+++ flutter_tutorial/multi_page_form_app/lib/step1.dart
@@ -9,16 +9,48 @@ class Step1 extends StatefulWidget {
 }
 
 class _Step1State extends State<Step1> {
+  final _formKey = GlobalKey<FormState>();
+  String _name = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Step 1'),
       ),
-      body: const Form(
+      body: Form(
+        key: _formKey,
         child: Padding(
-          padding: EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0),
           child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Name'),
+                onSaved: (value) {
+                  _name = value!;
+                },
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your name';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    Navigator.pushNamed(
+                      context,
+                      '/step2',
+                      arguments: {'name': _name},
+                    );
+                  }
+                },
+                child: const Text('Next'),
+              ),
+            ],
           ),
         ),
       ),
