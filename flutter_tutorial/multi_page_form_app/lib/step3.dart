88d753b611a415b0b37d52c68d175b464b31f712
pchchv <ipchchv@gmail.com>
2024-06-14 09:03:16 +0300

    Implement a Step3

--- flutter_tutorial/multi_page_form_app/lib/step3.dart
+++ flutter_tutorial/multi_page_form_app/lib/step3.dart
@@ -9,16 +9,54 @@ class Step3 extends StatefulWidget {
 }
 
 class _Step3State extends State<Step3> {
+  final _formKey = GlobalKey<FormState>();
+  String _phone = '';
+
   @override
   Widget build(BuildContext context) {
+    final Map arguments = ModalRoute.of(context)!.settings.arguments as Map;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Step 3'),
       ),
-      body: const Form(
+      body: Form(
+        key: _formKey,
         child: Padding(
-          padding: EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0),
           child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Phone'),
+                onSaved: (value) {
+                  _phone = value!;
+                },
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your phone number';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    Navigator.pushNamed(
+                      context,
+                      '/summary',
+                      arguments: {
+                        'name': arguments['name'],
+                        'email': arguments['email'],
+                        'phone': _phone,
+                      },
+                    );
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ],
           ),
         ),
       ),
