ddeec5147309c98ea16e92a926ea0a127b340bce
pchchv <ipchchv@gmail.com>
2024-06-14 09:03:28 +0300

    Implement a Step2

--- flutter_tutorial/multi_page_form_app/lib/step2.dart
+++ flutter_tutorial/multi_page_form_app/lib/step2.dart
@@ -9,16 +9,53 @@ class Step2 extends StatefulWidget {
 }
 
 class _Step2State extends State<Step2> {
+  final _formKey = GlobalKey<FormState>();
+  String _email = '';
+
   @override
   Widget build(BuildContext context) {
+    final Map arguments = ModalRoute.of(context)!.settings.arguments as Map;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Step 2'),
       ),
-      body: const Form(
+      body: Form(
+        key: _formKey,
         child: Padding(
-          padding: EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0),
           child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Email'),
+                onSaved: (value) {
+                  _email = value!;
+                },
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    Navigator.pushNamed(
+                      context,
+                      '/step3',
+                      arguments: {
+                        'name': arguments['name'],
+                        'email': _email,
+                      },
+                    );
+                  }
+                },
+                child: const Text('Next'),
+              ),
+            ],
           ),
         ),
       ),
