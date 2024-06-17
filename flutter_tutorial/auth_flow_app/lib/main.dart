764981bf2c495ed8af0cd49f1d1e92ebe888a048
pchchv <ipchchv@gmail.com>
2024-06-17 15:45:43 +0300

    Implement an AuthFlowApp

--- flutter_tutorial/auth_flow_app/lib/main.dart
+++ flutter_tutorial/auth_flow_app/lib/main.dart
@@ -1,2 +1,20 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+
 void main() {
+}
+
+class AuthFlowApp extends StatelessWidget {
+  const AuthFlowApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Authentication Flow',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginScreen(),
+    );
+  }
 }
