1ed548e4baf8126870467d1645e595f0d25ddb44
pchchv <ipchchv@gmail.com>
2024-06-17 08:49:27 +0300

    Implement a HomeScreen

--- flutter_tutorial/drawer_menu_app/lib/main.dart
+++ flutter_tutorial/drawer_menu_app/lib/main.dart
@@ -17,3 +17,19 @@ class MenuApp extends StatelessWidget {
     );
   }
 }
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('Home Screen'),
+      ),
+    );
+  }
+}
