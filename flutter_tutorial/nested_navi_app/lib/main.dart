8195c58f5deddfed54726a36812aa75b132454cd
pchchv <ipchchv@gmail.com>
2024-06-17 15:50:50 +0300

    Implement a NaviApp

--- flutter_tutorial/nested_navi_app/lib/main.dart
+++ flutter_tutorial/nested_navi_app/lib/main.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const NaviApp());
+}
+
+class NaviApp extends StatelessWidget {
+  const NaviApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Nested Navigation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
