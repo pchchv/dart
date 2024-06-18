fe8895de180ed2d65ac82d9118e9ed2374105a1d
pchchv <ipchchv@gmail.com>
2024-06-18 14:31:04 +0300

    Implement a ScreensApp

--- flutter_tutorial/transition_animation_app/lib/main.dart
+++ flutter_tutorial/transition_animation_app/lib/main.dart
@@ -1,2 +1,20 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const ScreensApp());
+}
+
+class ScreensApp extends StatelessWidget {
+  const ScreensApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Custom Transition App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+    );
+  }
 }
