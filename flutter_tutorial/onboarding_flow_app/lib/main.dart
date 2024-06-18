91a7ffc3b6dc7c31706ef8c31c33a855fcd44802
pchchv <ipchchv@gmail.com>
2024-06-18 14:50:35 +0300

    Add routes

--- flutter_tutorial/onboarding_flow_app/lib/main.dart
+++ flutter_tutorial/onboarding_flow_app/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'onboarding_screen_1.dart';
+import 'onboarding_screen_2.dart';
+import 'onboarding_screen_3.dart';
+import 'main_content.dart';
 
 void main() {
   runApp(const OnboardingApp());
@@ -9,8 +13,15 @@ class OnboardingApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Onboarding App',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const OnboardingScreen1(),
+        '/onboarding2': (context) => const OnboardingScreen2(),
+        '/onboarding3': (context) => const OnboardingScreen3(),
+        '/main': (context) => const MainContent(),
+      },
     );
   }
 }
