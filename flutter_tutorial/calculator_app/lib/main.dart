7b2c8c8b56e67286ed0ec6155cfb43c730aa3011
pchchv <ipchchv@gmail.com>
2024-06-19 12:37:08 +0300

    Implement an InterestCalculatorScreen

--- flutter_tutorial/calculator_app/lib/main.dart
+++ flutter_tutorial/calculator_app/lib/main.dart
@@ -11,6 +11,18 @@ class CalculatorApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Simple Interest Calculator',
+      home: InterestCalculatorScreen(),
     );
   }
+}
+
+class InterestCalculatorScreen extends StatefulWidget {
+  const InterestCalculatorScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _InterestCalculatorScreenState createState() => _InterestCalculatorScreenState();
+}
+
+class _InterestCalculatorScreenState extends State<InterestCalculatorScreen> {
 }
