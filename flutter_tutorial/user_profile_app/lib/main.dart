0a36d7aa2824c4f3581429ce5b171ada26e2cc59
pchchv <ipchchv@gmail.com>
2024-06-19 12:31:10 +0300

    Add info section

--- flutter_tutorial/user_profile_app/lib/main.dart
+++ flutter_tutorial/user_profile_app/lib/main.dart
@@ -51,6 +51,39 @@ class ProfilePage extends StatelessWidget {
               textAlign: TextAlign.center,
               style: Theme.of(context).textTheme.bodyLarge),
           const SizedBox(height: 20),
+          Card(
+            elevation: 4.0,
+            child: Column(
+              children: <Widget>[
+                const ListTile(
+                  leading: Icon(Icons.cake),
+                  title: Text('Birth Date'),
+                  subtitle: Text('1993-12-22'),
+                ),
+                ListTile(
+                    leading: const Icon(Icons.web),
+                    title: const Text('Website'),
+                    subtitle: const Text('pchchv.me'),
+                    onTap: () {}),
+                const ListTile(
+                  leading: Icon(Icons.email),
+                  title: Text('Email'),
+                  subtitle: Text('ipchchv@gmail.com'),
+                ),
+                const ListTile(
+                  leading: Icon(Icons.location_on),
+                  title: Text('Address'),
+                  subtitle: Text('Moscow, Russia'),
+                ),
+                const ListTile(
+                  leading: Icon(Icons.location_on),
+                  title: Text('Hobbies'),
+                  subtitle: Text('Cycling, triathlon, camping, bicycle travel'),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
         ],
       ),
     );
