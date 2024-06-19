63c0a652166453f9605309d20ee058fa362bc74a
pchchv <ipchchv@gmail.com>
2024-06-19 12:36:18 +0300

    Implement a CalculatorApp

--- flutter_tutorial/calculator_app/lib/main.dart
+++ flutter_tutorial/calculator_app/lib/main.dart
@@ -1,2 +1,15 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class CalculatorApp extends StatelessWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Simple Interest Calculator',
+    );
+  }
 }
