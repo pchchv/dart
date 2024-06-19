e7f59cc4d920b63eca6508b9ba3f53d736bcbfaf
pchchv <ipchchv@gmail.com>
2024-06-19 12:22:10 +0300

    Implement a ProfileApp

--- flutter_tutorial/user_profile_app/lib/main.dart
+++ flutter_tutorial/user_profile_app/lib/main.dart
@@ -1,2 +1,24 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class ProfileApp extends StatelessWidget {
+  const ProfileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Profile'),
+        ),
+      ),
+    );
+  }
 }
