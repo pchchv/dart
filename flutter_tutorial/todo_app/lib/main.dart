905ec9dd4efdd7dae28c5fa52489c9bb7dc8d599
pchchv <ipchchv@gmail.com>
2024-06-28 21:44:04 +0300

    Implement a ToDoApp

--- flutter_tutorial/todo_app/lib/main.dart
+++ flutter_tutorial/todo_app/lib/main.dart
@@ -1,2 +1,15 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Simple ToDo App',
+    );
+  }
 }
