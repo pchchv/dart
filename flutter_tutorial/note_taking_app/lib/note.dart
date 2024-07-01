43405eff38d2a0bd3c3a62ef75e47bc9f703b999
pchchv <ipchchv@gmail.com>
2024-07-01 13:32:28 +0300

    Implement a toMap factory

--- flutter_tutorial/note_taking_app/lib/note.dart
+++ flutter_tutorial/note_taking_app/lib/note.dart
@@ -6,4 +6,18 @@ class Note {
     required this.id,
     required this.content,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'content': content,
+    };
+  }
+
+  factory Note.fromMap(Map<String, dynamic> map) {
+    return Note(
+      id: map['id'],
+      content: map['content'],
+    );
+  }
 }
