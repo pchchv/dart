b0b23059a849749e0e381a593ae2c2495993720f
pchchv <ipchchv@gmail.com>
2024-07-01 13:30:45 +0300

    Implement a NoteTakingApp

--- flutter_tutorial/note_taking_app/lib/main.dart
+++ flutter_tutorial/note_taking_app/lib/main.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const NoteTakingApp());
+}
+
+class NoteTakingApp extends StatelessWidget {
+  const NoteTakingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Note Taking App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
