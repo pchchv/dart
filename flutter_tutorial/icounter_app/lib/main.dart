b88ff71ded7714ba559804d6b3b072f7138c3929
pchchv <ipchchv@gmail.com>
2024-07-01 13:19:21 +0300

    Implement a CounterPage

--- flutter_tutorial/icounter_app/lib/main.dart
+++ flutter_tutorial/icounter_app/lib/main.dart
@@ -14,6 +14,18 @@ class CounterApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: const CounterPage(),
     );
   }
+}
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _CounterPageState createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
 }
