346f4d926e8c756d54963199dd2e06a803951a52
pchchv <ipchchv@gmail.com>
2024-07-01 13:22:06 +0300

    Implement a counter display

--- flutter_tutorial/icounter_app/lib/main.dart
+++ flutter_tutorial/icounter_app/lib/main.dart
@@ -48,10 +48,17 @@ class _CounterPageState extends State<CounterPage> {
       appBar: AppBar(
         title: const Text('Counter App'),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
           ],
         ),
       ),
