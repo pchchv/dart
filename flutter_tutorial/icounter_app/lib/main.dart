8d369cf6d148862315dfe2f37892e3510fe98045
pchchv <ipchchv@gmail.com>
2024-07-01 13:18:37 +0300

    Implement a CounterApp

--- flutter_tutorial/icounter_app/lib/main.dart
+++ flutter_tutorial/icounter_app/lib/main.dart
@@ -1,2 +1,18 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class CounterApp extends StatelessWidget {
+  const CounterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
