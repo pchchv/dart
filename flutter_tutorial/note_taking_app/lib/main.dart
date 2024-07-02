8253ecdb29dc6b8801105894e2c99d6585f0ea8a
pchchv <ipchchv@gmail.com>
2024-07-02 11:32:39 +0300

    Implement a NotePage

--- flutter_tutorial/note_taking_app/lib/main.dart
+++ flutter_tutorial/note_taking_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'note_page.dart';
 
 void main() {
   runApp(const NoteTakingApp());
@@ -14,6 +15,7 @@ class NoteTakingApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: const NotePage(),
     );
   }
 }
---
+++ flutter_tutorial/note_taking_app/lib/note_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NotePage extends StatefulWidget {
+  const NotePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _NotePageState createState() => _NotePageState();
+}
+
+class _NotePageState extends State<NotePage> {
+}
