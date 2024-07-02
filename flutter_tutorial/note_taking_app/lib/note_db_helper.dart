d52a51c27b545eab1346b492238ab57245d3f4e6
pchchv <ipchchv@gmail.com>
2024-07-02 11:29:37 +0300

    Implement _initDB func

--- flutter_tutorial/note_taking_app/lib/note_db_helper.dart
+++ flutter_tutorial/note_taking_app/lib/note_db_helper.dart
@@ -1,4 +1,5 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 class NoteDbHelper {
   static final NoteDbHelper instance = NoteDbHelper._init();
   static Database? _database;
@@ -12,5 +13,23 @@ class NoteDbHelper {
       content TEXT NOT NULL
     )
     ''');
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await _initDB('notes.db');
+    return _database!;
   }
 }
