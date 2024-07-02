592ca9d2ff0e551cd5dbfca914ba8544f63969f4
pchchv <ipchchv@gmail.com>
2024-07-02 20:13:37 +0300

    Implement ConverterApp

--- flutter_tutorial/currency_converter_app/lib/main.dart
+++ flutter_tutorial/currency_converter_app/lib/main.dart
@@ -1,2 +1,18 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class ConverterApp extends StatelessWidget {
+  const ConverterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Currency Converter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
