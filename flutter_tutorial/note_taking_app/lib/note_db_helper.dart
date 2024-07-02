645ea69d1f946a7ee7d722979c2a9973e1351ef1
pchchv <ipchchv@gmail.com>
2024-07-02 11:28:55 +0300

    Implement _createDB func

--- flutter_tutorial/note_taking_app/lib/note_db_helper.dart
+++ flutter_tutorial/note_taking_app/lib/note_db_helper.dart
@@ -1,6 +1,16 @@
+import 'package:sqflite/sqflite.dart';
 class NoteDbHelper {
   static final NoteDbHelper instance = NoteDbHelper._init();
   static Database? _database;
 
   NoteDbHelper._init();
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE notes (
+      id INTEGER PRIMARY KEY,
+      content TEXT NOT NULL
+    )
+    ''');
+  }
 }
