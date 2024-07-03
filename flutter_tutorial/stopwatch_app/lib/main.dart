0fa657fc7687b4adde235fc56513de246fe95df9
pchchv <ipchchv@gmail.com>
2024-07-03 12:56:56 +0300

    Implement a StopwatchApp

--- flutter_tutorial/stopwatch_app/lib/main.dart
+++ flutter_tutorial/stopwatch_app/lib/main.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const StopwatchApp());
+}
+
+class StopwatchApp extends StatelessWidget {
+  const StopwatchApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stopwatch App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
