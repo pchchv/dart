1a682f5a15363ee23e47e743164fc391a550c028
pchchv <ipchchv@gmail.com>
2024-07-03 13:15:17 +0300

    Implement a CalculatorApp

--- flutter_tutorial/basic_calculator_app/lib/main.dart
+++ flutter_tutorial/basic_calculator_app/lib/main.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Basic Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
