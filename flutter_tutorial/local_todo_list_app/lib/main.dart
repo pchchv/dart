520dee47f7ff7cb4769e4c2775d76fcf3fb4ffc5
pchchv <ipchchv@gmail.com>
2024-07-05 19:25:50 +0300

    Implement a TodoApp

--- flutter_tutorial/local_todo_list_app/lib/main.dart
+++ flutter_tutorial/local_todo_list_app/lib/main.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To-Do List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
