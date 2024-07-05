02eec24703309c43722ecc2f5fa049839614df4d
pchchv <ipchchv@gmail.com>
2024-07-05 08:42:13 +0300

    Implement _loadPreferences and _savePreference funcs

--- flutter_tutorial/settings_page_app/lib/settings_page.dart
+++ flutter_tutorial/settings_page_app/lib/settings_page.dart
@@ -1,3 +1,4 @@
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatefulWidget {
@@ -9,6 +10,22 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  bool _isDarkMode = false;
+  late SharedPreferences _prefs;
+  bool _notificationsEnabled = false;
+
+  Future<void> _loadPreferences() async {
+    _prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _isDarkMode = _prefs.getBool('isDarkMode') ?? false;
+      _notificationsEnabled = _prefs.getBool('notificationsEnabled') ?? false;
+    });
+  }
+
+  Future<void> _savePreference(String key, bool value) async {
+    await _prefs.setBool(key, value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
