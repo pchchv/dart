af01594e4242b9a23e5a1dfb889d1dd297da4bc3
pchchv <ipchchv@gmail.com>
2024-07-05 08:37:34 +0300

    Implement a SettingsApp

--- flutter_tutorial/settings_page_app/lib/main.dart
+++ flutter_tutorial/settings_page_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,9 +10,18 @@ class SettingsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const FutureBuilder(
-      future: null,
-      builder: (){},
+    return FutureBuilder(
+      future: SharedPreferences.getInstance(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          final prefs = snapshot.data as SharedPreferences;
+          final isDarkMode = prefs.getBool('isDarkMode') ?? false;
+          return MaterialApp(
+            theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
+          );
+        }
+        return const CircularProgressIndicator();
+      },
     );
   }
 }
