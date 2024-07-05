dd28c55c6fb9ba8341cb9b7aaea614d4c470064e
pchchv <ipchchv@gmail.com>
2024-07-05 08:34:05 +0300

    Implement a SettingsApp

--- flutter_tutorial/settings_page_app/lib/main.dart
+++ flutter_tutorial/settings_page_app/lib/main.dart
@@ -1,2 +1,17 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const SettingsApp());
+}
+
+class SettingsApp extends StatelessWidget {
+  const SettingsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const FutureBuilder(
+      future: null,
+      builder: (){},
+    );
+  }
 }
