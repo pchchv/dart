5c1f7128ce8f059e1358e5d477f699a6ed4862d5
pchchv <ipchchv@gmail.com>
2024-07-08 13:59:58 +0300

    Implement _UserProfileScreenState

--- flutter_tutorial/profile_screen_app/lib/user_profile_screen.dart
+++ flutter_tutorial/profile_screen_app/lib/user_profile_screen.dart
@@ -9,4 +9,19 @@ class UserProfileScreen extends StatefulWidget {
 }
 
 class _UserProfileScreenState extends State<UserProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Profile'),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+          ],
+        ),
+      ),
+    );
+  }
 }
