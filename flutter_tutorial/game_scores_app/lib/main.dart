5601f1644f525b0b70b3d34f9c50e1198066ba7e
pchchv <ipchchv@gmail.com>
2024-07-08 14:30:26 +0300

    Implement a GameApp

--- flutter_tutorial/game_scores_app/lib/main.dart
+++ flutter_tutorial/game_scores_app/lib/main.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const GameApp());
+}
+
+class GameApp extends StatelessWidget {
+  const GameApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Game Score Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
