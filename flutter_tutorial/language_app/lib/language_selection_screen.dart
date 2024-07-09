2a0f6c17ceabec302d9b53fb0e117522edf155f5
pchchv <ipchchv@gmail.com>
2024-07-09 12:57:57 +0300

    Implement a LanguageSelectionScreen

---
+++ flutter_tutorial/language_app/lib/language_selection_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LanguageSelectionScreen extends StatefulWidget {
+  const LanguageSelectionScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LanguageSelectionScreenState createState() => _LanguageSelectionScreenState();
+}
+
+class _LanguageSelectionScreenState extends State<LanguageSelectionScreen> {
+}
