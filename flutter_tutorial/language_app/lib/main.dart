d3fb59c2b97351410ebc02920a454482e3f7cfb0
pchchv <ipchchv@gmail.com>
2024-07-09 12:55:48 +0300

    Implement LanguageApp

--- flutter_tutorial/language_app/lib/main.dart
+++ flutter_tutorial/language_app/lib/main.dart
@@ -1,2 +1,17 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class LanguageApp extends StatelessWidget {
+  final String? preferredLanguage;
+
+  const LanguageApp({super.key, this.preferredLanguage});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: Locale(preferredLanguage ?? 'en'),
+    );
+  }
 }
