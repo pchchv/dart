50eeae053604da2400798474c60b96a2543c08ac
pchchv <ipchchv@gmail.com>
2024-07-09 13:01:46 +0300

    Implement locales

--- flutter_tutorial/language_app/lib/main.dart
+++ flutter_tutorial/language_app/lib/main.dart
@@ -1,5 +1,8 @@
+import 'generated/l10n.dart';
 import 'package:flutter/material.dart';
+import 'language_selection_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,6 +20,14 @@ class LanguageApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       locale: Locale(preferredLanguage ?? 'en'),
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      home: const LanguageSelectionScreen(),
     );
   }
 }
