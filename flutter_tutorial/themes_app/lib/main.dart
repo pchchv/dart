be54a0fed3915ec173ab8de52551e8883df21ff5
pchchv <ipchchv@gmail.com>
2024-07-10 12:28:57 +0300

    Implement a ThemeApp

--- flutter_tutorial/themes_app/lib/main.dart
+++ flutter_tutorial/themes_app/lib/main.dart
@@ -1,2 +1,22 @@
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'theme_manager.dart';
+
 void main() {
+}
+
+class ThemeApp extends StatelessWidget {
+  const ThemeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeNotifier>(
+      builder: (context, themeNotifier, child) {
+        return MaterialApp(
+          title: 'Theme Switcher',
+          theme: themeNotifier.darkTheme ? ThemeData.dark() : ThemeData.light(),
+        );
+      },
+    );
+  }
 }
