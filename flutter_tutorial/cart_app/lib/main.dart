45b1c8c7c23a5f257b86217b6e3ec3101593a9a7
pchchv <ipchchv@gmail.com>
2024-07-11 13:05:24 +0300

    Implement a ShoppingApp

--- flutter_tutorial/cart_app/lib/main.dart
+++ flutter_tutorial/cart_app/lib/main.dart
@@ -1,2 +1,16 @@
 void main() {
 }
+
+class ShoppingApp extends StatelessWidget {
+  const ShoppingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopping App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
