28ac0cf7a5dac3d805bfabc80ebfbede97f9ad9a
pchchv <ipchchv@gmail.com>
2024-07-12 09:26:58 +0300

    Implement an OnboardingApp

--- flutter_tutorial/onboarding_app/lib/main.dart
+++ flutter_tutorial/onboarding_app/lib/main.dart
@@ -1,2 +1,21 @@
-void main() {
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(OnboardingApp(seenOnboarding: seenOnboarding));
+}
+
+class OnboardingApp extends StatelessWidget {
+  final bool seenOnboarding;
+
+  const OnboardingApp({super.key, required this.seenOnboarding});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Onboarding Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
