f421c4baec3325ad910a2e4c52331739ceb93843
pchchv <ipchchv@gmail.com>
2024-07-12 08:41:31 +0300

    Add NotesApp and app runner

--- flutter_tutorial/cat_notes_app/lib/main.dart
+++ flutter_tutorial/cat_notes_app/lib/main.dart
@@ -1,2 +1,13 @@
-void main() {
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+  }
 }
