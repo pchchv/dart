7d0e2f667747ae3bf5fb75e76ff657014afc714c
pchchv <ipchchv@gmail.com>
2024-07-12 09:29:41 +0300

    Add preferences to app runner

--- flutter_tutorial/onboarding_app/lib/main.dart
+++ flutter_tutorial/onboarding_app/lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool? seenOnboarding = prefs.getBool('seenOnboarding') ?? false;
+
   runApp(OnboardingApp(seenOnboarding: seenOnboarding));
 }
 
