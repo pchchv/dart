3f15070a973bf040c0445262d970816699f53008
pchchv <ipchchv@gmail.com>
2024-07-12 09:05:20 +0300

    Implement a HomeScreen

---
+++ flutter_tutorial/cat_notes_app/lib/screens/home_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/note_provider.dart';
+import 'note_detail_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notes'),
+      ),
+      body: Consumer<NoteProvider>(
+        builder: (context, noteProvider, child) {
+          return ListView.builder(
+            itemCount: noteProvider.notes.length,
+            itemBuilder: (context, index) {
+              final note = noteProvider.notes[index];
+              return ListTile(
+                title: Text(note.title),
+                subtitle: Text(note.category),
+                onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => NoteDetailScreen(note: note),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const NoteDetailScreen(),
+          ),
+        ),
+      ),
+    );
+  }
+}
