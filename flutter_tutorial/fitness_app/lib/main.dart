709a7d668180b2ebd69fc955317e249a05d4ba81
pchchv <ipchchv@gmail.com>
2024-07-15 11:48:09 +0300

    Implement a FitnessApp

--- flutter_tutorial/fitness_app/lib/main.dart
+++ flutter_tutorial/fitness_app/lib/main.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const FitnessApp());
+}
+
+class FitnessApp extends StatelessWidget {
+  const FitnessApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fitness App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
