8e17cfc44615bf8cbd5088ad9dde3126cc0238be
pchchv <ipchchv@gmail.com>
2024-07-16 14:04:53 +0300

    Implement a fromJson factory

--- flutter_tutorial/motiv_quotes_app/lib/quote.dart
+++ flutter_tutorial/motiv_quotes_app/lib/quote.dart
@@ -3,4 +3,11 @@ class Quote {
   final String author;
 
   Quote({required this.text, required this.author});
+
+  factory Quote.fromJson(Map<String, dynamic> json) {
+    return Quote(
+      text: json['quote'] ?? '',
+      author: json['author'] ?? 'Unknown',
+    );
+  }
 }
