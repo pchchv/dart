45356aec894dec1aeee2fa65054bb148f168d4fd
pchchv <ipchchv@gmail.com>
2024-07-16 14:24:44 +0300

    Implement FetchApp

--- flutter_tutorial/fetches_data_app/lib/main.dart
+++ flutter_tutorial/fetches_data_app/lib/main.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const FetchApp());
+}
+
+class FetchApp extends StatelessWidget {
+  const FetchApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'User List App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
