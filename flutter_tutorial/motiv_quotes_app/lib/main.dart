00c10bcbe0d850ae4502e2e0d5aa236f432e9fb8
pchchv <ipchchv@gmail.com>
2024-07-16 14:01:58 +0300

    Implement a QuotesApp

--- flutter_tutorial/motiv_quotes_app/lib/main.dart
+++ flutter_tutorial/motiv_quotes_app/lib/main.dart
@@ -1,2 +1,18 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class QuotesApp extends StatelessWidget {
+  const QuotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Motivational Quotes',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
