6e62f1ed162bafad5ddc03f0763f58d6782c3d7e
pchchv <ipchchv@gmail.com>
2024-07-17 15:37:58 +0300

    Implement a WeatherApp

--- flutter_tutorial/weather_app/lib/main.dart
+++ flutter_tutorial/weather_app/lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter/material.dart';
 
 void main() async {
   await dotenv.load();
+  runApp(const WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
