d16a00e06fb02221b7ccd0d13694ab0c2e36c024
pchchv <ipchchv@gmail.com>
2024-07-17 15:23:49 +0300

    Implement a MessageApp

--- flutter_tutorial/user_message_app/lib/main.dart
+++ flutter_tutorial/user_message_app/lib/main.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const MessageApp());
+}
+
+class MessageApp extends StatelessWidget {
+  const MessageApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Message Form',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
