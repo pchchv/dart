e2f75abf9c22d87e5ca44bcf8302d3279b134f79
pchchv <ipchchv@gmail.com>
2024-07-17 15:28:35 +0300

    Implement a form submitter

--- flutter_tutorial/user_message_app/lib/main.dart
+++ flutter_tutorial/user_message_app/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 void main() {
   runApp(const MessageApp());
@@ -28,6 +30,40 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _messageController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  Future<void> _submitForm() async {
+    if (_formKey.currentState!.validate()) {
+      final String name = _nameController.text;
+      final String message = _messageController.text;
+
+      final response = await http.post(
+        Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'name': name,
+          'message': message,
+        }),
+      );
+
+      if (response.statusCode == 201) {
+        // ignore: use_build_context_synchronously
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Submission successful!')),
+        );
+      } else {
+        // ignore: use_build_context_synchronously
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Submission failed!')),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
