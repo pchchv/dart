228099ec8ed6a739801bbc5a408234cca267cc2f
pchchv <ipchchv@gmail.com>
2024-07-18 14:57:33 +0300

    Implement a WeatherService

---
+++ flutter_tutorial/weather_app/lib/weather_service.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class WeatherService {
+  final String apiKey = dotenv.env['API_KEY']!;
+
+  Future<Map<String, dynamic>> fetchWeather(String city) async {
+    final url = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+}
