8bc624287c9a1e8c638b89500a2b9a6643403403
pchchv <ipchchv@gmail.com>
2024-07-19 11:17:31 +0300

    Add basic app

--- flutter_tutorial/another_todo_app/lib/main.dart
+++ flutter_tutorial/another_todo_app/lib/main.dart
@@ -1,2 +1,82 @@
-void main() {
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => Todo(),
+      child: const MaterialApp(
+        home: TodoPage(),
+      ),
+    );
+  }
+}
+
+class TodoPage extends StatelessWidget {
+  const TodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final todo = Provider.of<Todo>(context);
+    TextEditingController todoController = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Provider Todo App'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              labelText: 'Type todo and hit enter',
+              contentPadding: EdgeInsets.all(10),
+            ),
+            controller: todoController,
+            onSubmitted: (value) {
+              todo.add(value);
+              todoController.clear();
+            },
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: todo.todos.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(todo.todos[index]),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      todo.remove(todo.todos[index]);
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class Todo extends ChangeNotifier {
+  final List<String> _todos = [];
+
+  List<String> get todos => _todos;
+
+  void add(String todo) {
+    _todos.add(todo);
+    notifyListeners();
+  }
+
+  void remove(String todo) {
+    _todos.remove(todo);
+    notifyListeners();
+  }
 }
