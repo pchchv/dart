9442b87ef020adf1ecc95660dc5995d0058ced74
pchchv <ipchchv@gmail.com>
2024-07-19 12:30:25 +0300

    Create the Add Task Screen

--- flutter_tutorial/simple_todo_app/lib/task_list.dart
+++ flutter_tutorial/simple_todo_app/lib/task_list.dart
@@ -1,6 +1,7 @@
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'task_data.dart';
+import 'task_tile.dart';
 
 class TaskList extends StatelessWidget {
   const TaskList({super.key});
@@ -8,6 +9,24 @@ class TaskList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            final task = taskData.tasks[index];
+            return TaskTile(
+              taskTitle: task.name,
+              isChecked: task.isDone,
+              checkboxCallback: (checkboxState) {
+                taskData.updateTask(task);
+              },
+              longPressCallback: () {
+                taskData.deleteTask(task);
+              },
+            );
+          },
+          itemCount: taskData.tasks.length,
+        );
+      },
     );
   }
 }
