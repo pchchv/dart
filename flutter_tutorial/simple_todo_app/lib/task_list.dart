cf7014e42f767771aac44a44d636ed38c60a9e12
pchchv <ipchchv@gmail.com>
2024-07-19 12:30:14 +0300

    Add a TaskList

---
+++ flutter_tutorial/simple_todo_app/lib/task_list.dart
@@ -0,0 +1,13 @@
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'task_data.dart';
+
+class TaskList extends StatelessWidget {
+  const TaskList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskData>(
+    );
+  }
+}
