6e6674a252d75725c7e162ac1fc3789004b1c1a7
pchchv <ipchchv@gmail.com>
2024-07-19 12:38:14 +0300

    Implement a tasks widget

--- flutter_tutorial/simple_todo_app/lib/screens/task_screen.dart
+++ flutter_tutorial/simple_todo_app/lib/screens/task_screen.dart
@@ -1,4 +1,6 @@
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
+import '../task_data.dart';
 
 class TaskScreen extends StatelessWidget {
   const TaskScreen({super.key});
@@ -17,6 +19,39 @@ class TaskScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
+            padding: const EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: Icon(
+                    Icons.list,
+                    size: 30.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                const Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '${Provider.of<TaskData>(context).tasks.length} Tasks',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
           ),
           Expanded(
             child: Container(
