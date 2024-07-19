d01312ac145723d59b0ffff31bae5d3dee6532ae
pchchv <ipchchv@gmail.com>
2024-07-19 12:42:22 +0300

    Create the Add Task Screen

--- flutter_tutorial/simple_todo_app/lib/screens/add_task_screen.dart
+++ flutter_tutorial/simple_todo_app/lib/screens/add_task_screen.dart
@@ -1,11 +1,58 @@
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
+import '/task_data.dart';
 
 class AddTaskScreen extends StatelessWidget {
   const AddTaskScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    String newTaskTitle = '';
+
     return Container(
+      color: const Color(0xff757575),
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+              onChanged: (newText) {
+                newTaskTitle = newText;
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Provider.of<TaskData>(context, listen: false).addTask(newTaskTitle);
+                Navigator.pop(context);
+              },
+              child: const Text(
+                'Add',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
