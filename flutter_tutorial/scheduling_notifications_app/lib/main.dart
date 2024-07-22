d4d988b264b4a94c2bf3cf169f8786d277931a78
pchchv <ipchchv@gmail.com>
2024-07-22 12:29:11 +0300

    Implement a NotifyApp

--- flutter_tutorial/scheduling_notifications_app/lib/main.dart
+++ flutter_tutorial/scheduling_notifications_app/lib/main.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const NotifyApp());
+}
+
+class NotifyApp extends StatelessWidget {
+  const NotifyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Local Notifications App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
