4d13d62abd525ad49ee6a1058501d33190df2f0a
pchchv <ipchchv@gmail.com>
2024-07-22 12:53:58 +0300

    Update dependencies

--- flutter_tutorial/scheduling_notifications_app/lib/main.dart
+++ flutter_tutorial/scheduling_notifications_app/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void main() {
+  tz.initializeTimeZones();
   runApp(const NotifyApp());
 }
 
