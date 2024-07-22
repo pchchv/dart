dd32f5802b6f5a243ee2a45cc8e74243c7b5036e
pchchv <ipchchv@gmail.com>
2024-07-22 12:02:41 +0300

    Implement a ThemesApp

--- flutter_tutorial/theme_provider_app/lib/main.dart
+++ flutter_tutorial/theme_provider_app/lib/main.dart
@@ -1,2 +1,14 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class ThemesApp extends StatelessWidget {
+  const ThemesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+    );
+  }
 }
