a44b381c1b0ecee0a9d6df94707fefb1c9f37a03
pchchv <ipchchv@gmail.com>
2024-07-23 13:25:21 +0300

    Implement _scheduleNotification and _addTask funcs

--- flutter_tutorial/reminders_tasks_app/lib/main.dart
+++ flutter_tutorial/reminders_tasks_app/lib/main.dart
@@ -1,7 +1,13 @@
+import 'task.dart';
+import 'provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void main() {
+  tz.initializeTimeZones();
   runApp(TasksApp());
 }
 
@@ -25,6 +31,39 @@ class _HomePageState extends State<HomePage> {
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
   final TextEditingController _taskController = TextEditingController();
   DateTime? _selectedDateTime;
+
+  Future<void> _scheduleNotification(Task task) async {
+    const androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'task_reminder_channel',
+      'Task Reminders',
+      importance: Importance.max,
+      priority: Priority.high,
+      showWhen: false,
+    );
+    const iOSPlatformChannelSpecifics = DarwinNotificationDetails();
+    const platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: iOSPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      task.hashCode,
+      'Task Reminder',
+      task.title,
+      tz.TZDateTime.from(task.dateTime, tz.local),
+      platformChannelSpecifics,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+    );
+  }
+
+  void _addTask() {
+    if (_taskController.text.isNotEmpty && _selectedDateTime != null) {
+      final task = Task(
+          title: _taskController.text, dateTime: _selectedDateTime!);
+      Provider.of<TaskProvider>(context, listen: false).addTask(task);
+      _scheduleNotification(task);
+    }
+  }
 
   Future<void> _selectDateTime(BuildContext context) async {
     final DateTime? pickedDate = await showDatePicker(
