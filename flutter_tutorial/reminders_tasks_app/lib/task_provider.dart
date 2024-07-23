2e4340b3d2f827d2aa4d298c3db8472d67280570
pchchv <ipchchv@gmail.com>
2024-07-23 13:01:01 +0300

    Implement a removeTask and addTask functions

--- flutter_tutorial/reminders_tasks_app/lib/task_provider.dart
+++ flutter_tutorial/reminders_tasks_app/lib/task_provider.dart
@@ -5,4 +5,14 @@ class TaskProvider extends ChangeNotifier {
   final List<Task> _tasks = [];
 
   List<Task> get tasks => _tasks;
+
+  void addTask(Task task) {
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void removeTask(Task task) {
+    _tasks.remove(task);
+    notifyListeners();
+  }
 }
