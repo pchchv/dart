7c5fdb6e350d25ebc8dbd6988552dd6248d637db
pchchv <ipchchv@gmail.com>
2024-07-23 12:57:27 +0300

    Add TasksApp

--- flutter_tutorial/reminders_tasks_app/lib/main.dart
+++ flutter_tutorial/reminders_tasks_app/lib/main.dart
@@ -1,2 +1,11 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(TasksApp());
+}
+
+class TasksApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+  }
 }
