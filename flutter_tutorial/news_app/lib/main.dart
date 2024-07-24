ef898e3b4693e18b1f21e871aecd012d284ac359
pchchv <ipchchv@gmail.com>
2024-07-24 14:52:01 +0300

    Implement a NewsApp

--- flutter_tutorial/news_app/lib/main.dart
+++ flutter_tutorial/news_app/lib/main.dart
@@ -1,2 +1,19 @@
+import 'package:flutter/material.dart';
+
 void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
