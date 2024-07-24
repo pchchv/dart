6bb5563d1eae7c7bbb91f4f100ccd0da92692ba9
pchchv <ipchchv@gmail.com>
2024-07-24 14:57:33 +0300

    Implement _HomePageState

--- flutter_tutorial/news_app/lib/home_page.dart
+++ flutter_tutorial/news_app/lib/home_page.dart
@@ -9,4 +9,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('News App'),
+      ),
+      body: const Center(
+        child: Text('Welcome to the News App!'),
+      ),
+    );
+  }
 }
