6551eb1b7269216ebdd009db7d0ff8eb8ed7af9b
pchchv <ipchchv@gmail.com>
2024-07-24 15:04:28 +0300

    Implement fetching news articles

--- flutter_tutorial/news_app/lib/article.dart
+++ flutter_tutorial/news_app/lib/article.dart
@@ -1,3 +1,17 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<List<Article>> fetchArticles() async {
+  final response = await http.get(Uri.parse('')); // Add API URL
+
+  if (response.statusCode == 200) {
+    final List<dynamic> articlesJson = json.decode(response.body)['articles'];
+    return articlesJson.map((json) => Article.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed to load articles');
+  }
+}
+
 class Article {
   final String title;
   final String description;
