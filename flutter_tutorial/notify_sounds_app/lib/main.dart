ac524b68d6e3c17f45aecc1a2e9643525021c6b3
pchchv <ipchchv@gmail.com>
2024-07-24 15:14:17 +0300

    Implement a NotifyApp

--- flutter_tutorial/notify_sounds_app/lib/main.dart
+++ flutter_tutorial/notify_sounds_app/lib/main.dart
@@ -1,2 +1,18 @@
+import 'package:flutter/material.dart';
+
 void main() {
+}
+
+class NotifyApp extends StatelessWidget {
+  const NotifyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notification Sound Selector',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
