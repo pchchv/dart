5e6d169133294c11e6ee74b132a6c08b105f31d3
pchchv <ipchchv@gmail.com>
2024-07-24 15:00:45 +0300

    Configure FCM to handle push notifications

--- flutter_tutorial/news_app/lib/home_page.dart
+++ flutter_tutorial/news_app/lib/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,6 +12,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      // Handle foreground message
+      if (message.notification != null) {
+        log('Message also contained a notification: ${message.notification}');
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      // Handle when the app is opened from a notification
+      log('Message clicked!');
+    });
+
+    _firebaseMessaging.getToken().then((String? token) {
+      assert(token != null);
+      log("Push Messaging token: $token");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
