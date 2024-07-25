4eb3d20b545fd8a33a7daa83e89138190d9c1ffa
pchchv <ipchchv@gmail.com>
2024-07-25 12:58:21 +0300

    Implement a _pickSound function

--- flutter_tutorial/notify_sounds_app/lib/main.dart
+++ flutter_tutorial/notify_sounds_app/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 void main() {
   runApp(const NotifyApp());
@@ -29,6 +32,7 @@ class SoundSelectorPage extends StatefulWidget {
 }
 
 class _SoundSelectorPageState extends State<SoundSelectorPage> {
+  late String customSoundPath;
   late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
   Future<void> _showNotification(String soundPath) async {
@@ -48,6 +52,19 @@ class _SoundSelectorPageState extends State<SoundSelectorPage> {
     await flutterLocalNotificationsPlugin.show(
         0, 'Custom Sound Notification', 'This is the body of the notification', platformChannelSpecifics,
         payload: 'item x');
+  }
+
+  Future<void> _pickSound() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.audio);
+    if (result != null && result.files.single.path != null) {
+      File file = File(result.files.single.path!); // Use the non-nullable path
+      Directory appDocDir = await getApplicationDocumentsDirectory();
+      String appDocPath = appDocDir.path;
+      final File newFile = await file.copy('$appDocPath/${result.files.single.name}');
+      setState(() {
+        customSoundPath = newFile.path;
+      });
+    }
   }
 
   @override
