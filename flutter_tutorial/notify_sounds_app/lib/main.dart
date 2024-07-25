3333156474a3fe442842c468937a0c3dd3dd57c1
pchchv <ipchchv@gmail.com>
2024-07-25 12:44:19 +0300

    Implement a SoundSelectorPage

--- flutter_tutorial/notify_sounds_app/lib/main.dart
+++ flutter_tutorial/notify_sounds_app/lib/main.dart
@@ -13,6 +13,18 @@ class NotifyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: const SoundSelectorPage(),
     );
   }
+}
+
+class SoundSelectorPage extends StatefulWidget {
+  const SoundSelectorPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SoundSelectorPageState createState() => _SoundSelectorPageState();
+}
+
+class _SoundSelectorPageState extends State<SoundSelectorPage> {
 }
