c4303ebc4be9830c00c24f4130f3b4be9b3d53dc
pchchv <ipchchv@gmail.com>
2024-07-26 12:17:09 +0300

    Implement a MyHomePage

--- flutter_tutorial/notify_actions_app/lib/main.dart
+++ flutter_tutorial/notify_actions_app/lib/main.dart
@@ -14,6 +14,18 @@ class NotifyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(),
     );
   }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
 }
